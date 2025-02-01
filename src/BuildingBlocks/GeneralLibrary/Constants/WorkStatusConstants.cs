f03bc6616b93b3692fe21ad979ffeb17c0a90515
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-01 18:16:13 +0400

    Add constant

---
+++ src/BuildingBlocks/GeneralLibrary/Constants/WorkStatusConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace GeneralLibrary.Constants
+{
+    public class WorkStatusConstants
+    {
+        public const string LookingForJob = "Активно ищу работу";
+        public const string NotLookingForJob = "Не ищу работу";
+        public const string ConsideringOffers = "Рассматриваю предложения";
+    }
+}
