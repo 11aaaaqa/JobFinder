cc53fdabf371404a92e7819041f346d88d6e5703
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-01 19:49:49 +0400

    Add migrations

---
+++ src/Microservices/Employee/EmployeeMicroservice.Api/Database/Migrations/20250201153254_Initt.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_PhoneNumber",
+                table: "Employees",
+                column: "PhoneNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_PhoneNumber",
+                table: "Employees");
+        }
+    }
+}
