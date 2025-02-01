76577b0470ac55a1ebf122389077fe9a918d04c8
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-01 18:16:30 +0400

    Update models

--- src/Microservices/Employee/EmployeeMicroservice.Api/Models/Employee.cs
+++ src/Microservices/Employee/EmployeeMicroservice.Api/Models/Employee.cs
@@ -1,6 +1,4 @@
-﻿using EmployeeMicroservice.Api.Models.EmployeeSkills;
-
-namespace EmployeeMicroservice.Api.Models
+﻿namespace EmployeeMicroservice.Api.Models
 {
     public class Employee
     {
@@ -8,17 +6,11 @@ namespace EmployeeMicroservice.Api.Models
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
-        public DateOnly DateOfBirth { get; set; }
-        public bool ReadyToMove { get; set; }
+        public string? Patronymic { get; set; }
+        public string? Gender { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
         public string? City { get; set; }
         public string? PhoneNumber { get; set; }
-        public string? Post { get; set; }
-        public uint? DesiredSalaryFrom { get; set; }
-        public uint? DesiredSalaryTo { get; set; }
-        public string? OccupationType { get; set; }
-        public string? AboutMe { get; set; }
-        public List<Education> Educations { get; set; }
-        public List<EmployeeExperience> EmployeeExperience { get; set; }
-        public List<ForeignLanguage> ForeignLanguages { get; set; }
+        public string Status { get; set; }
     }
 }
--- src/Microservices/Employee/EmployeeMicroservice.Api/Models/EmployeeSkills/ForeignLanguage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmployeeMicroservice.Api.Models.EmployeeSkills
-{
-    public class ForeignLanguage
-    {
-        public Guid Id { get; set; }
-        public string LanguageName { get; set; }
-        public string LanguageProficiencyLevel { get; set; }
-    }
-}
