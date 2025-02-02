1beaf985dc4967ae7c6e5ff283ad75098125e1a3
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-02 14:09:08 +0400

    Add models

---
+++ src/Web/Web.MVC/DTOs/Employee/UpdateEmployeeDto.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.Employee
+{
+    public class UpdateEmployeeDto
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Имя\" обязательно")]
+        [Display(Name = "Имя")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Фамилия\" обязательно")]
+        [Display(Name = "Фамилия")]
+        [StringLength(50)]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Адрес эл. почты\" обязательно")]
+        [Display(Name = "Адрес эл. почты")]
+        [StringLength(80)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Отчество")]
+        [StringLength(50)]
+        public string? Patronymic { get; set; }
+
+        [Display(Name = "Пол")]
+        public string? Gender { get; set; }
+
+        [Display(Name = "Дата рождения")]
+        [DataType(DataType.Date)]
+        public DateOnly? DateOfBirth { get; set; }
+
+        [Display(Name = "Город проживания")]
+        [StringLength(50)]
+        public string? City { get; set; }
+
+        [Display(Name = "Номер телефона")]
+        [StringLength(20)]
+        [DataType(DataType.PhoneNumber)]
+        public string? PhoneNumber { get; set; }
+    }
+}
---
+++ src/Web/Web.MVC/Models/ApiResponses/EmployeeResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Web.MVC.Models.ApiResponses
+{
+    public class EmployeeResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string? Patronymic { get; set; }
+        public string? Gender { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
+        public string? City { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string Status { get; set; }
+    }
+}
