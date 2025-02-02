070f0eba418db4052884e097604d66cd4e8529e1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-02 14:21:56 +0400

    Add migrations

---
+++ src/Microservices/Employee/EmployeeMicroservice.Api/Database/Migrations/20250202101937_AddAccountIdColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountIdColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccountId",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "Employees");
+        }
+    }
+}
