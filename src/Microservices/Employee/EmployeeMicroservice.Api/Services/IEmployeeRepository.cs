83492a06d3902d60c9eadc7b97166498cb0f5c3e
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-02 14:08:53 +0400

    Add employee repository

---
+++ src/Microservices/Employee/EmployeeMicroservice.Api/Services/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeMicroservice.Api.Models;
+
+namespace EmployeeMicroservice.Api.Services
+{
+    public interface IEmployeeRepository
+    {
+        Task<Employee?> GetEmployeeByIdAsync(Guid id);
+        Task<Employee?> GetEmployeeByEmailAsync(string email);
+        Task UpdateEmployeeAsync(Employee employee);
+    }
+}
