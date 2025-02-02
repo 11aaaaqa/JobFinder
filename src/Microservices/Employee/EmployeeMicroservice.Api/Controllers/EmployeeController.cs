5b95e0317644230aee7eea5f5f7b75b6f3fec4cd
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-02 14:09:12 +0400

    Add controller

---
+++ src/Microservices/Employee/EmployeeMicroservice.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeMicroservice.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMicroservice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController(IEmployeeRepository employeeRepository) : ControllerBase
+    {
+        [Route("GetEmployeeById/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeeByIdAsync(Guid id)
+        {
+            var employee = await employeeRepository.GetEmployeeByIdAsync(id);
+            if (employee is null) return BadRequest();
+
+            return Ok(employee);
+        }
+
+        [Route("GetEmployeeByEmail/{email}")]
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeeByEmailAsync(string email)
+        {
+            var employee = await employeeRepository.GetEmployeeByEmailAsync(email);
+            if (employee is null) return BadRequest();
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ src/Web/Web.MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Web.MVC.DTOs.Employee;
+using Web.MVC.Models.ApiResponses;
+
+namespace Web.MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly string url;
+        public EmployeeController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            this.httpClientFactory = httpClientFactory;
+            url = $"{configuration["Url:Protocol"]}://{configuration["Url:Domain"]}";
+        }
+
+        [Authorize]
+        [Route("profile/me")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateEmployee()
+        {
+            using HttpClient httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync($"{url}/api/Employee/GetEmployeeByEmail/{User.Identity.Name}");
+            response.EnsureSuccessStatusCode();
+
+            var employee = await response.Content.ReadFromJsonAsync<EmployeeResponse>();
+            return View(new UpdateEmployeeDto
+            {
+                Name = employee.Name, Email = employee.Email, City = employee.City, DateOfBirth = employee.DateOfBirth,
+                Gender = employee.Gender, Id = employee.Id, Patronymic = employee.Patronymic,
+                PhoneNumber = employee.PhoneNumber, Surname = employee.Surname
+            });
+        }
+
+        [Authorize]
+        [Route("profile/me")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateEmployee(UpdateEmployeeDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+
+            return View(model);
+        }
+    }
+}
