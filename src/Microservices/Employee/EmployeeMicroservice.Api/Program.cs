610f44eb104204fca0a05d2236dc26fa851782f8
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-02 15:50:59 +0400

    Add kafka producer class

--- src/Microservices/Employee/EmployeeMicroservice.Api/Program.cs
+++ src/Microservices/Employee/EmployeeMicroservice.Api/Program.cs
@@ -1,5 +1,6 @@
 using EmployeeMicroservice.Api.Database;
 using EmployeeMicroservice.Api.Kafka;
+using EmployeeMicroservice.Api.Kafka.Kafka_producer;
 using EmployeeMicroservice.Api.Services;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(x =>
     x.UseNpgsql(builder.Configuration["Database:ConnectionString"]));
 
+builder.Services.AddScoped<IKafkaProducer, KafkaProducer>();
 builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
 
 builder.Services.AddHostedService<UserRegisteredKafkaConsumer>();
