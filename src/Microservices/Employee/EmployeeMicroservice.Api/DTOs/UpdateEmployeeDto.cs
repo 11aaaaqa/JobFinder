abfda26a9c9983c25eb112852be4154f3279f494
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-03 20:38:14 +0400

    add dto

---
+++ src/Microservices/Employee/EmployeeMicroservice.Api/DTOs/UpdateEmployeeDto.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeMicroservice.Api.DTOs
+{
+    public class UpdateEmployeeDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string? Patronymic { get; set; }
+        public string? Gender { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
+        public string? City { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string Status { get; set; }
+    }
+}
