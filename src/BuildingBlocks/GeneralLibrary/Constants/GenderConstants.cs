552980af9657ebd2824266752ef2589e4a082afb
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-04 19:01:50 +0400

    Add constants

---
+++ src/BuildingBlocks/GeneralLibrary/Constants/GenderConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace GeneralLibrary.Constants
+{
+    public class GenderConstants
+    {
+        public const string Man = "Мужской";
+        public const string Woman = "Женский";
+    }
+}
