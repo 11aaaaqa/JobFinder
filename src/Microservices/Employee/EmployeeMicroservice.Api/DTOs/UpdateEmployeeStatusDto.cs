ef7203f94155825cdfd72714a0e5b2e4a2b4cc7c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-10 16:26:39 +0400

    add dto

---
+++ src/Microservices/Employee/EmployeeMicroservice.Api/DTOs/UpdateEmployeeStatusDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeMicroservice.Api.DTOs
+{
+    public class UpdateEmployeeStatusDto
+    {
+        public Guid EmployeeId { get; set; }
+        public string Status { get; set; }
+    }
+}
