5964f5e93c48e73075a8d214b4a8f0f978933948
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-11 18:54:30 +0400

    update

--- src/Web/Web.MVC/wwwroot/js/resume/get-my-resumes.js
+++ src/Web/Web.MVC/wwwroot/js/resume/get-my-resumes.js
@@ -1,12 +1,20 @@
 ﻿const button = document.getElementById('status');
 const modal = document.getElementById('update-status-modal');
+const rightBlock = document.getElementById('status');
 
-button.onclick = function() {
+button.onclick = function () {
+    var rect = rightBlock.getBoundingClientRect();
+    var containerRect = document.querySelector('.profile-section').getBoundingClientRect();
+
+    modal.style.top = (rect.bottom - containerRect.top) + "px";
+    modal.style.left = (rect.left - containerRect.left) + "px";
+    modal.style.width = rect.width + "px";
+
     modal.style.display = modal.style.display === 'block' ? 'none' : 'block';
 };
 
 window.onclick = function(event) {
-    if (event.target !== button) {
+    if (event.target !== button && event.target !== modal) {
         modal.style.display = 'none';
     }
 };
