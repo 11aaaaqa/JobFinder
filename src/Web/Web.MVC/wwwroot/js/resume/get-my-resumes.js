7de033eb79af647410639336d681503b53288080
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-11 17:45:48 +0400

    add script

---
+++ src/Web/Web.MVC/wwwroot/js/resume/get-my-resumes.js
@@ -0,0 +1,12 @@
+﻿const button = document.getElementById('status');
+const modal = document.getElementById('update-status-modal');
+
+button.onclick = function() {
+    modal.style.display = modal.style.display === 'block' ? 'none' : 'block';
+};
+
+window.onclick = function(event) {
+    if (event.target !== button) {
+        modal.style.display = 'none';
+    }
+};
