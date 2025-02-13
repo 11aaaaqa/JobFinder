9e3a87fdca7aac9654e035076e3cd5b99ad955e8
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-13 20:04:08 +0400

    Add employer repository implementation

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Services/EmployerRepository.cs
@@ -0,0 +1,36 @@
+﻿using EmployerMicroservice.Api.Database;
+using EmployerMicroservice.Api.DTOs;
+using EmployerMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployerMicroservice.Api.Services
+{
+    public class EmployerRepository(ApplicationDbContext context) : IEmployerRepository
+    {
+        public async Task<Employer?> GetEmployerByIdAsync(Guid id)
+            => await context.Employers.SingleOrDefaultAsync(x => x.Id == id);
+
+        public async Task<Employer?> GetEmployerByEmailAsync(string email)
+            => await context.Employers.SingleOrDefaultAsync(x => x.Email == email);
+
+        public async Task UpdateEmployerAsync(UpdateEmployerDto model)
+        {
+            var employer = await context.Employers.SingleOrDefaultAsync(x => x.Id == model.Id);
+            if (employer is null) throw new Exception("Employer with current ID wasn't found");
+
+            employer.Name = model.Name; employer.Surname = model.Surname; employer.CompanyPost = model.CompanyPost;
+
+            await context.SaveChangesAsync();
+        }
+
+        public async Task AssignCompanyAsync(Guid employerId, Guid companyId)
+        {
+            var employer = await context.Employers.SingleOrDefaultAsync(x => x.Id == employerId);
+            if (employer is null) throw new Exception("Employer with current ID wasn't found");
+
+            employer.CompanyId = companyId;
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
