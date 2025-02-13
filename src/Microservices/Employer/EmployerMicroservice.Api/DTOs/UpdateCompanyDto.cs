c583cd771e798d678db7c00b7fcc885c2b7638c4
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-13 19:53:58 +0400

    add dto

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/DTOs/UpdateCompanyDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployerMicroservice.Api.DTOs
+{
+    public class UpdateCompanyDto
+    {
+        public Guid Id { get; set; }
+        public string CompanyName { get; set; }
+        public string? CompanyDescription { get; set; }
+        public uint CompanyColleaguesCount { get; set; }
+    }
+}
