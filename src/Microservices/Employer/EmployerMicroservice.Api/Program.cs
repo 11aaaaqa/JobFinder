4ba0a0a7de179871a84baf3ed095bfa58ece86ed
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-13 20:04:16 +0400

    register dependencies

--- src/Microservices/Employer/EmployerMicroservice.Api/Program.cs
+++ src/Microservices/Employer/EmployerMicroservice.Api/Program.cs
@@ -1,11 +1,15 @@
 using EmployerMicroservice.Api.Database;
 using EmployerMicroservice.Api.Kafka.Consumers;
+using EmployerMicroservice.Api.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(
     x => x.UseNpgsql(builder.Configuration["Database:ConnectionString"]));
+
+builder.Services.AddTransient<IEmployerRepository, EmployerRepository>();
+builder.Services.AddTransient<ICompanyRepository, CompanyRepository>();
 
 builder.Services.AddHostedService<UserRegisteredKafkaConsumer>();
 builder.Services.AddControllers();
