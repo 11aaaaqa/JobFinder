119ecfc0dd8968e6661f41af95c14450468b64e1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-13 19:54:25 +0400

    add employer repository

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Services/IEmployerRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployerMicroservice.Api.DTOs;
+using EmployerMicroservice.Api.Models;
+
+namespace EmployerMicroservice.Api.Services
+{
+    public interface IEmployerRepository
+    {
+        Task<Employer?> GetEmployerByIdAsync(Guid id);
+        Task<Employer?> GetEmployerByEmailAsync(string email);
+        Task UpdateEmployerAsync(UpdateEmployerDto model);
+        Task AssignCompanyAsync(Guid companyId);
+    }
+}
