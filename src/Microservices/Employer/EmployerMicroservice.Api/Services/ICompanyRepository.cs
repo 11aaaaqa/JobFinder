23ce8d19043346346fb69f87f47734f286d9dea9
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-13 19:54:19 +0400

    add company repository

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Services/ICompanyRepository.cs
@@ -0,0 +1,14 @@
+﻿using EmployerMicroservice.Api.DTOs;
+using EmployerMicroservice.Api.Models;
+
+namespace EmployerMicroservice.Api.Services
+{
+    public interface ICompanyRepository
+    {
+        Task<Company?> GetCompanyByIdAsync(Guid id);
+        Task<Company?> GetCompanyByCompanyNameAsync(string companyName);
+        Task AddCompanyAsync(Company model);
+        Task UpdateCompanyAsync(UpdateCompanyDto model);
+        Task DeleteCompanyAsync(Guid id);
+    }
+}
