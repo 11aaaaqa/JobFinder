edb190944a9064605f0ce1a6bb4e87892a3a0f26
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-14 18:37:28 +0400

    Update method name

--- src/Microservices/Employer/tests/EmployerMicroservice.UnitTests/CompanyControllerUnitTests.cs
+++ src/Microservices/Employer/tests/EmployerMicroservice.UnitTests/CompanyControllerUnitTests.cs
@@ -41,7 +41,46 @@ namespace EmployerMicroservice.UnitTests
             var methodResult = Assert.IsType<OkObjectResult>(result);
             Assert.NotNull(methodResult.Value);
             var addedCompany = Assert.IsType<Company>(methodResult.Value);
-            Assert.Equal(companyId, addedCompany.Id);
+            Assert.Equal(company, addedCompany);
+            mock.VerifyAll();
+        }
+
+        [Fact]
+        public async Task GetCompanyByCompanyNameAsync_ReturnsBadRequest()
+        {
+            var companyName = "TestCompanyName";
+            var mock = new Mock<ICompanyRepository>();
+            mock.Setup(x => x.GetCompanyByCompanyNameAsync(companyName)).ReturnsAsync((Company?)null);
+            var controller = new CompanyController(mock.Object);
+
+            var result = await controller.GetCompanyByCompanyNameAsync(companyName);
+
+            Assert.IsType<BadRequestResult>(result);
+            mock.VerifyAll();
+        }
+
+        [Fact]
+        public async Task GetCompanyByCompanyNameAsync_ReturnsOk()
+        {
+            var companyName = "TestCompanyName";
+            var company = new Company
+            {
+                Id = Guid.NewGuid(),
+                CompanyName = companyName,
+                CompanyDescription = It.IsAny<string>(),
+                CompanyColleaguesCount = It.IsAny<uint>(),
+                FounderEmployerId = Guid.NewGuid()
+            };
+            var mock = new Mock<ICompanyRepository>();
+            mock.Setup(x => x.GetCompanyByCompanyNameAsync(companyName)).ReturnsAsync(company);
+            var controller = new CompanyController(mock.Object);
+
+            var result = await controller.GetCompanyByCompanyNameAsync(companyName);
+
+            var methodResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(methodResult.Value);
+            var addedCompany = Assert.IsType<Company>(methodResult.Value);
+            Assert.Equal(company, addedCompany);
             mock.VerifyAll();
         }
     }
