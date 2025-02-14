9849117395e6418ad6f9547295db1bd408e4aa77
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-14 16:49:06 +0400

    Add DTOs

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/DTOs/AddCompanyDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployerMicroservice.Api.DTOs
+{
+    public class AddCompanyDto
+    {
+        public Guid FounderEmployerId { get; set; }
+        public string CompanyName { get; set; }
+        public string? CompanyDescription { get; set; }
+        public uint CompanyColleaguesCount { get; set; }
+    }
+}
