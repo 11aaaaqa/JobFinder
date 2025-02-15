309de898f5f0c727c87c43d3482ab0a87a27fa69
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-15 17:52:50 +0400

    add dto

---
+++ src/Web/Web.MVC/DTOs/Employer/UpdateEmployerDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.Employer
+{
+    public class UpdateEmployerDto
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Имя\" обязательно")]
+        [Display(Name = "Имя")]
+        [StringLength(30, ErrorMessage = "Максимальная длина поля \"Имя\" - 30 символов")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Фамилия\" обязательно")]
+        [Display(Name = "Фамилия")]
+        [StringLength(30, ErrorMessage = "Максимальная длина поля \"Фамилия\" - 30 символов")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Должность")]
+        [StringLength(50, ErrorMessage = "Максимальная длина поля \"Должность\" - 50 символов")]
+        public string? CompanyPost { get; set; }
+    }
+}
