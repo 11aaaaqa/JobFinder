8f6907310228f69d737206f71195d95b93cd90b7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-15 17:53:37 +0400

    Add script

---
+++ src/Web/Web.MVC/wwwroot/js/employer/employer-successfully-updated.js
@@ -0,0 +1,16 @@
+﻿const successInput = document.getElementById("isUpdated");
+
+const isUpdated = successInput.value;
+const modal = document.getElementById('modal');
+
+if (isUpdated === 'true') {
+    modal.classList.add('show');
+    setTimeout(() => {
+        modal.classList.remove('show');
+    }, 3000);
+}
+
+const span = document.getElementById('close-modal');
+span.onclick = function () {
+    modal.classList.remove('show');
+};
