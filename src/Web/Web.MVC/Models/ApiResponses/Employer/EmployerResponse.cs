1bc3c72dd0aaf4b20696df26a6d9947e6691ba5a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-15 17:53:00 +0400

    Add api response model

---
+++ src/Web/Web.MVC/Models/ApiResponses/Employer/EmployerResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.MVC.Models.ApiResponses.Employer
+{
+    public class EmployerResponse
+    {
+        public Guid Id { get; set; }
+        public string AccountId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string? CompanyPost { get; set; }
+        public Guid? CompanyId { get; set; }
+    }
+}
