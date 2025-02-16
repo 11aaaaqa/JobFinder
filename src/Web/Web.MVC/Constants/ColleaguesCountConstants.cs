be3c5ec686d8a7d33dd86f530ff23b1ded0513a4
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-16 17:27:12 +0400

    Add constants

---
+++ src/Web/Web.MVC/Constants/ColleaguesCountConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Web.MVC.Constants
+{
+    public class ColleaguesCountConstants
+    {
+        public const string UnderFifty = "До 50";
+        public const string FiftyToOneHundred = "50 - 100";
+        public const string OneHundredToFiveHundred = "100 - 500";
+        public const string FiveHundredToOneThousand = "500 - 1000";
+        public const string OneThousandToFiveThousand = "1000 - 5000";
+        public const string MoreThanFiveThousand = "Более 5000";
+    }
+}
