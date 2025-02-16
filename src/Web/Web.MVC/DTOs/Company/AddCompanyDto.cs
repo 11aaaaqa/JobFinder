17a96b9f5df596be2aabf22a4353e153ff52bdb6
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-16 17:26:33 +0400

    Add dto

---
+++ src/Web/Web.MVC/DTOs/Company/AddCompanyDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.Company
+{
+    public class AddCompanyDto
+    {
+        public Guid FounderEmployerId { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Название компании\" обязательно")]
+        [Display(Name = "Название компании")]
+        [StringLength(50)]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "Описание")]
+        public string? CompanyDescription { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Количество сотрудников\" обязательно")]
+        [Display(Name = "Количество сотрудников")]
+        public string CompanyColleaguesCount { get; set; }
+    }
+}
