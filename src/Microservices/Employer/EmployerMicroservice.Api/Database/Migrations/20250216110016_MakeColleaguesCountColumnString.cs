65bcf9692e7688740e53ff6a8ba614e16366085b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-16 17:26:15 +0400

    Add migration

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Database/Migrations/20250216110016_MakeColleaguesCountColumnString.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployerMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeColleaguesCountColumnString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyColleaguesCount",
+                table: "Companies",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "CompanyColleaguesCount",
+                table: "Companies",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
