006e29d1d5f602c42e2f50ab231efabce6c0ddaa
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-17 13:21:53 +0400

    Divide employer microservice into employer and company microservices

---
+++ src/Microservices/Company/CompanyMicroservice.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CompanyMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyMicroservice.Api.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Company> Companies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Company>().HasIndex(x => x.CompanyName).IsUnique();
+        }
+    }
+}
--- src/Microservices/Employer/EmployerMicroservice.Api/Database/ApplicationDbContext.cs
+++ src/Microservices/Employer/EmployerMicroservice.Api/Database/ApplicationDbContext.cs
@@ -8,13 +8,5 @@ namespace EmployerMicroservice.Api.Database
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Employer> Employers { get; set; }
-        public DbSet<Company> Companies { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-            builder.Entity<Company>().HasIndex(x => x.CompanyName).IsUnique();
-        }
     }
 }
