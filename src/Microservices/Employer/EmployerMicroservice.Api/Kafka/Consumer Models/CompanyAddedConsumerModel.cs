4ee92a58a6c99a85f8f2ca3d8f30de1d91310e02
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-17 14:38:01 +0400

    Add model

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Kafka/Consumer Models/CompanyAddedConsumerModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployerMicroservice.Api.Kafka.Consumer_Models
+{
+    public class CompanyAddedConsumerModel
+    {
+        public Guid CompanyId { get; set; }
+        public Guid EmployerId { get; set; }
+    }
+}
