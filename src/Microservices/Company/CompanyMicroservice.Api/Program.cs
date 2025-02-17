8eb6ed337cb244c156a7f5e876a994c568256de7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-17 14:37:47 +0400

    Register kafka producer

--- src/Microservices/Company/CompanyMicroservice.Api/Program.cs
+++ src/Microservices/Company/CompanyMicroservice.Api/Program.cs
@@ -1,4 +1,5 @@
 using CompanyMicroservice.Api.Database;
+using CompanyMicroservice.Api.Kafka.Producer;
 using CompanyMicroservice.Api.Services;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(
     x => x.UseNpgsql(builder.Configuration["Database:ConnectionString"]));
 
 builder.Services.AddTransient<ICompanyRepository, CompanyRepository>();
+builder.Services.AddTransient<IKafkaProducer, KafkaProducer>();
 
 builder.Services.AddControllers();
 
