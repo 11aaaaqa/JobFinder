31bddd2cc2064488c7bed57a733063dad46a60ec
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-17 14:37:38 +0400

    Add kafka producer class

---
+++ src/Microservices/Company/CompanyMicroservice.Api/Kafka/Producer/IKafkaProducer.cs
@@ -0,0 +1,9 @@
+﻿using Confluent.Kafka;
+
+namespace CompanyMicroservice.Api.Kafka.Producer
+{
+    public interface IKafkaProducer
+    {
+        Task ProduceAsync(string topic, Message<Null, string> message);
+    }
+}
