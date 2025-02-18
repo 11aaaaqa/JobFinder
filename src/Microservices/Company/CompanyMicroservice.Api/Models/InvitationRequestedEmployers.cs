8d7ee6873587df2b2cbd0154a521b886d8e0215c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-18 17:37:56 +0400

    Add model

---
+++ src/Microservices/Company/CompanyMicroservice.Api/Models/InvitationRequestedEmployers.cs
@@ -0,0 +1,12 @@
+﻿namespace CompanyMicroservice.Api.Models
+{
+    public class InvitationRequestedEmployers
+    {
+        public Guid Id { get; set; }
+        public Guid CompanyId { get; set; }
+        public Guid EmployerId { get; set; }
+        public string EmployerName { get; set; }
+        public string EmployerSurname { get; set; }
+        public DateTime JoiningRequestDate { get; set; }
+    }
+}
