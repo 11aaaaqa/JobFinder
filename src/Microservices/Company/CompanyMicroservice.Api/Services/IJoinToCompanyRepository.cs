17b3bc4ef6a2c56de8f10b6dcec32141c39f6ea5
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-20 19:01:53 +0400

    Add repository

---
+++ src/Microservices/Company/CompanyMicroservice.Api/Services/IJoinToCompanyRepository.cs
@@ -0,0 +1,12 @@
+﻿using CompanyMicroservice.Api.Models;
+
+namespace CompanyMicroservice.Api.Services
+{
+    public interface IJoinToCompanyRepository
+    {
+        Task<List<JoiningRequestedEmployer>> GetJoiningRequestedEmployersByCompanyIdAsync(Guid companyId);
+        Task AddJoiningRequestAsync(JoiningRequestedEmployer model);
+        Task AcceptRequestAsync(Guid requestId);
+        Task RejectRequestAsync(Guid requestId);
+    }
+}
