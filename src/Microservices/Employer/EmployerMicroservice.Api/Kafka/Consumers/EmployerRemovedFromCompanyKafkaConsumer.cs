590b3176dfcc5cc287474c281deddf84181d1c23
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-21 20:09:36 +0400

    Add kafka consumer

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Kafka/Consumers/EmployerRemovedFromCompanyKafkaConsumer.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.Json;
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using EmployerMicroservice.Api.Constants;
+using EmployerMicroservice.Api.Database;
+using EmployerMicroservice.Api.Kafka.Consumer_Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployerMicroservice.Api.Kafka.Consumers
+{
+    public class EmployerRemovedFromCompanyKafkaConsumer(IConfiguration configuration, IServiceScopeFactory scopeFactory) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            const string topicName = "employer-removed-from-company-topic";
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = configuration["Kafka:BootstrapServers"],
+                GroupId = KafkaConstants.GroupId,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+            consumer.Subscribe(topicName);
+
+            using var scope = scopeFactory.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                ConsumeResult<Null, string> consumerResult = new();
+                try
+                {
+                    consumerResult = consumer.Consume(stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    if (!ex.Message.ToLower().Contains("unknown topic"))
+                        throw;
+                    using var adminClient = new AdminClientBuilder(config).Build();
+
+                    try
+                    {
+                        await adminClient.CreateTopicsAsync(new List<TopicSpecification>{
+                            new() { Name = topicName, NumPartitions = 1, ReplicationFactor = 1}});
+                    }
+                    catch (Exception exc)
+                    {
+                        if(!exc.Message.ToLower().Contains("already exists"))
+                            throw;
+                    }
+                    continue;
+                }
+
+                var model = JsonSerializer.Deserialize<EmployerRemovedFromCompanyModel>(consumerResult.Message.Value);
+                var employer = await context.Employers.SingleAsync(x => x.Id == model.EmployerId, CancellationToken.None);
+
+                employer.CompanyId = null;
+                await context.SaveChangesAsync(CancellationToken.None);
+            }
+            consumer.Close();
+        }
+    }
+}
