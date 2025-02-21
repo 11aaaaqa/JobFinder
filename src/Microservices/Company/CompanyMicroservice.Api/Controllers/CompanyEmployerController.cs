bbc7e0aca56d3eca61b41524dbe08ad18bc265c7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-21 19:02:29 +0400

    Add controller

---
+++ src/Microservices/Company/CompanyMicroservice.Api/Controllers/CompanyEmployerController.cs
@@ -0,0 +1,27 @@
+﻿using CompanyMicroservice.Api.DTOs;
+using CompanyMicroservice.Api.Services;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using CompanyMicroservice.Api.Kafka.Producer;
+
+namespace CompanyMicroservice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyEmployerController(IKafkaProducer kafkaProducer, ICompanyEmployerRepository companyEmployerRepository) : ControllerBase
+    {
+        [HttpPatch]
+        [Route("RemoveEmployerFromCompany")]
+        public async Task<IActionResult> RemoveEmployerFromCompanyAsync([FromBody] RemoveEmployerFromCompanyDto model)
+        {
+            await companyEmployerRepository.RemoveEmployerFromCompany(model.CompanyId, model.EmployerId);
+
+            await kafkaProducer.ProduceAsync("employer-removed-from-company-topic", new Message<Null, string>
+            {
+                Value = JsonSerializer.Serialize(new { model.EmployerId })
+            });
+            return Ok();
+        }
+    }
+}
