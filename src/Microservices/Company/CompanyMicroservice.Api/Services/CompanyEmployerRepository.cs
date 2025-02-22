0d13cff58a049f55bea39c41556f0840af9e2d36
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-22 20:11:31 +0400

    Implement requesting joining company method

--- src/Microservices/Company/CompanyMicroservice.Api/Services/CompanyEmployerRepository.cs
+++ src/Microservices/Company/CompanyMicroservice.Api/Services/CompanyEmployerRepository.cs
@@ -1,15 +1,26 @@
 ﻿using CompanyMicroservice.Api.Database;
+using CompanyMicroservice.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CompanyMicroservice.Api.Services
 {
     public class CompanyEmployerRepository(ApplicationDbContext context) : ICompanyEmployerRepository
     {
-        public async Task RemoveEmployerFromCompany(Guid companyId, Guid employerId)
+        public async Task RemoveEmployerFromCompanyAsync(Guid companyId, Guid employerId)
         {
             var company = await context.Companies.SingleAsync(x => x.Id == companyId);
 
             company.CompanyEmployersIds.Remove(employerId);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task RequestJoiningCompanyAsync(Guid companyId, Guid employerId, string employerName, string employerSurname)
+        {
+            await context.JoiningRequestedEmployers.AddAsync(new JoiningRequestedEmployer
+            {
+                Id = Guid.NewGuid(), CompanyId = companyId, EmployerId = employerId, EmployerName = employerName, EmployerSurname = employerSurname,
+                JoiningRequestDate = DateTime.UtcNow
+            });
             await context.SaveChangesAsync();
         }
     }
