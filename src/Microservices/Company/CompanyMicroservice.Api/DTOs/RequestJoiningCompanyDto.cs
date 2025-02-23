ab248f1895a7f2415f3cebe68d19d9b098e74847
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-23 13:51:22 +0400

    Add model

---
+++ src/Microservices/Company/CompanyMicroservice.Api/DTOs/RequestJoiningCompanyDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyMicroservice.Api.DTOs
+{
+    public class RequestJoiningCompanyDto
+    {
+        public Guid CompanyId { get; set; }
+        public Guid EmployerId { get; set; }
+        public string EmployerName { get; set; }
+        public string EmployerSurname { get; set; }
+    }
+}
