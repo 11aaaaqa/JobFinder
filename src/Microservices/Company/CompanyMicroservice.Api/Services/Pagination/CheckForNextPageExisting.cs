00fd9762bde4de4eff92385d8328aca0b945cb54
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-23 16:26:33 +0400

    Add pagination service

---
+++ src/Microservices/Company/CompanyMicroservice.Api/Services/Pagination/CheckForNextPageExisting.cs
@@ -0,0 +1,16 @@
+﻿using CompanyMicroservice.Api.Constants;
+using CompanyMicroservice.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyMicroservice.Api.Services.Pagination
+{
+    public class CheckForNextPageExisting(ApplicationDbContext context) : ICheckForNextPageExisting
+    {
+        public async Task<bool> DoesNextEmployersRequestedJoiningPageExist(Guid companyId, int pageNumber)
+        {
+            int startingCount = await context.JoiningRequestedEmployers.Where(x => x.CompanyId == companyId)
+                .Skip((pageNumber - 1) * PaginationConstants.EmployersPageSize).CountAsync();
+            return startingCount > 0;
+        }
+    }
+}
