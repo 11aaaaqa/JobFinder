682de1a0b9e968f6d34be242377f021f6e4ad61d
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-02-23 16:26:38 +0400

    Register dependency

--- src/Microservices/Company/CompanyMicroservice.Api/Program.cs
+++ src/Microservices/Company/CompanyMicroservice.Api/Program.cs
@@ -1,6 +1,7 @@
 using CompanyMicroservice.Api.Database;
 using CompanyMicroservice.Api.Kafka.Producer;
 using CompanyMicroservice.Api.Services;
+using CompanyMicroservice.Api.Services.Pagination;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(
     x => x.UseNpgsql(builder.Configuration["Database:ConnectionString"]));
 
+builder.Services.AddTransient<ICheckForNextPageExisting, CheckForNextPageExisting>();
 builder.Services.AddTransient<ICompanyEmployerRepository, CompanyEmployerRepository>();
 builder.Services.AddTransient<IJoinToCompanyRepository, JoinToCompanyRepository>();
 builder.Services.AddTransient<ICompanyRepository, CompanyRepository>();
