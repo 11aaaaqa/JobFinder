e3f8cd6e368e5f7bf815f52ca92a2653ca6c3423
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-07 20:06:13 +0400

    Add repos method

--- src/Microservices/Employer/EmployerMicroservice.Api/Services/IEmployerRepository.cs
+++ src/Microservices/Employer/EmployerMicroservice.Api/Services/IEmployerRepository.cs
@@ -7,6 +7,7 @@ namespace EmployerMicroservice.Api.Services
     {
         Task<Employer?> GetEmployerByIdAsync(Guid id);
         Task<Employer?> GetEmployerByEmailAsync(string email);
+        Task<List<Employer>> GetEmployersByCompanyId(Guid companyId, int pageNumber);
         Task<bool> UpdateEmployerAsync(UpdateEmployerDto model);
         Task<bool> AssignCompanyAsync(Guid employerId, Guid companyId);
     }
