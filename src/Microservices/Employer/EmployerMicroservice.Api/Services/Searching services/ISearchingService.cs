ef34d489b7309130ff653628613c3a5490b2a6e4
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-09 18:27:19 +0400

    Add searching service

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Services/Searching services/ISearchingService.cs
@@ -0,0 +1,9 @@
+﻿using EmployerMicroservice.Api.Models;
+
+namespace EmployerMicroservice.Api.Services.Searching_services
+{
+    public interface ISearchingService
+    {
+        Task<List<Employer>> FindEmployersAsync(Guid companyId,int pageNumber, string query);
+    }
+}
---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Services/Searching services/SearchingService.cs
@@ -0,0 +1,22 @@
+﻿using EmployerMicroservice.Api.Constants;
+using EmployerMicroservice.Api.Database;
+using EmployerMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployerMicroservice.Api.Services.Searching_services
+{
+    public class SearchingService(ApplicationDbContext context) : ISearchingService
+    {
+        public async Task<List<Employer>> FindEmployersAsync(Guid companyId,int pageNumber, string query)
+        {
+            var lowerQuery = query.ToLower();
+            var employers = await context.Employers.Where(x => x.CompanyId == companyId)
+                .Where(x => x.Name.ToLower().Contains(lowerQuery) | x.Surname.ToLower().Contains(lowerQuery) |
+                            x.Email.ToLower().Contains(lowerQuery) | x.CompanyPost.ToLower().Contains(lowerQuery))
+                .Skip((pageNumber - 1) * PaginationConstants.CompanyEmployersCountConstant)
+                .Take(PaginationConstants.CompanyEmployersCountConstant)
+                .ToListAsync();
+            return employers;
+        }
+    }
+}
