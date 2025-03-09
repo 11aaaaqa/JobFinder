fb0c39cf787e17378ed451c7598c74f3a1b7a1ed
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-09 18:26:26 +0400

    Add checking for next page existing method

--- src/Microservices/Employer/EmployerMicroservice.Api/Services/Pagination/IPaginationService.cs
+++ src/Microservices/Employer/EmployerMicroservice.Api/Services/Pagination/IPaginationService.cs
@@ -3,5 +3,6 @@
     public interface IPaginationService
     {
         Task<bool> DoesNextEmployersByCompanyPageExist(Guid companyId, int currentPageNumber);
+        Task<bool> DoesNextSearchingEmployersPageExist(Guid companyId, int currentPageNumber, string searchingQuery);
     }
 }
