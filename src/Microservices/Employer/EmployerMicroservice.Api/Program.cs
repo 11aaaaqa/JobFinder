ea4170b41b0ae4d9c471bbed3f2f237d3b93af44
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-09 18:27:29 +0400

    Register searching dependency

--- src/Microservices/Employer/EmployerMicroservice.Api/Program.cs
+++ src/Microservices/Employer/EmployerMicroservice.Api/Program.cs
@@ -2,6 +2,7 @@ using EmployerMicroservice.Api.Database;
 using EmployerMicroservice.Api.Kafka.Consumers;
 using EmployerMicroservice.Api.Services;
 using EmployerMicroservice.Api.Services.Pagination;
+using EmployerMicroservice.Api.Services.Searching_services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(
     x => x.UseNpgsql(builder.Configuration["Database:ConnectionString"]));
 
+builder.Services.AddTransient<ISearchingService, SearchingService> ();
 builder.Services.AddTransient<IPaginationService, PaginationService>();
 builder.Services.AddTransient<IEmployerRepository, EmployerRepository>();
 
