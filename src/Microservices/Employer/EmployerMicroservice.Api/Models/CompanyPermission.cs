b5f9f72e7de258c1a87335ef174c2bb0e2ff8547
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-16 20:42:22 +0400

    Add model

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Models/CompanyPermission.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployerMicroservice.Api.Models
+{
+    public class CompanyPermission
+    {
+        public Guid Id { get; set; }
+        public Guid EmployerId { get; set; }
+        public Guid CompanyId { get; set; }
+        public List<string> Permissions { get; set; }
+    }
+}
