8c9d37bc1914880d190ff87912af1ff69e1abcaa
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-16 20:42:16 +0400

    Add a custom exception

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Exceptions/CompanyPermissionDoesntExistException.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployerMicroservice.Api.Exceptions
+{
+    public class CompanyPermissionDoesntExistException : Exception
+    {
+        public CompanyPermissionDoesntExistException() { }
+        public CompanyPermissionDoesntExistException(string message) : base(message) { }
+    }
+}
