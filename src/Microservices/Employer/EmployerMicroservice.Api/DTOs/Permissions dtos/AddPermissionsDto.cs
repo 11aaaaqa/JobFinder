95294738665d0048f6feb851052d1af3a6320460
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-17 19:40:27 +0400

    Add dto

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/DTOs/Permissions dtos/AddPermissionsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployerMicroservice.Api.DTOs.Permissions_dtos
+{
+    public class AddPermissionsDto
+    {
+        public Guid EmployerId { get; set; }
+        public Guid CompanyId { get; set; }
+        public List<string> Permissions { get; set; }
+    }
+}
