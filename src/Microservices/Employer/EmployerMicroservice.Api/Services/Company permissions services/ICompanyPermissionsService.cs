e55882250af703f7b9f8b91434a61dc15fc6314c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-17 19:40:35 +0400

    Add permissions service

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Services/Company permissions services/ICompanyPermissionsService.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployerMicroservice.Api.Services.Company_permissions_services
+{
+    public interface ICompanyPermissionsService
+    {
+        Task<List<string>> GetAllPermissionsAsync(Guid employerId, Guid companyId);
+        Task<bool> CheckForPermissionAsync(Guid employerId, Guid companyId, string permissionName);
+        Task AddPermissions(Guid employerId, Guid companyId, List<string> permissions);
+    }
+}
