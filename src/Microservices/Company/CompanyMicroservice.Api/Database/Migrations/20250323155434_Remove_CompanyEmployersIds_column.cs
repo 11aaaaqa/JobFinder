24156330888c01b4964ce9d3a92b9ce6e81684de
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-23 20:17:25 +0400

    Add migration

---
+++ src/Microservices/Company/CompanyMicroservice.Api/Database/Migrations/20250323155434_Remove_CompanyEmployersIds_column.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_CompanyEmployersIds_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompanyEmployersIds",
+                table: "Companies");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<Guid>>(
+                name: "CompanyEmployersIds",
+                table: "Companies",
+                type: "uuid[]",
+                nullable: false);
+        }
+    }
+}
