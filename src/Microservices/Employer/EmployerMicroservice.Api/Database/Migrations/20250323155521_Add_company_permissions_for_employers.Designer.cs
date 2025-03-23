81d8f3561bd070c4d5763692d93304a446954ea9
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-23 20:17:31 +0400

    Add migration

--- src/Microservices/Employer/EmployerMicroservice.Api/Database/Migrations/20250323155521_Add_company_permissions_for_employers.Designer.cs
+++ src/Microservices/Employer/EmployerMicroservice.Api/Database/Migrations/20250323155521_Add_company_permissions_for_employers.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using EmployerMicroservice.Api.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EmployerMicroservice.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250323155521_Add_company_permissions_for_employers")]
+    partial class Add_company_permissions_for_employers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Database/Migrations/20250323155521_Add_company_permissions_for_employers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployerMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_company_permissions_for_employers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployersPermissions",
+                columns: table => new
+                {
+                    EmployerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Permissions = table.Column<List<string>>(type: "text[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployersPermissions", x => x.EmployerId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployersPermissions");
+        }
+    }
+}
