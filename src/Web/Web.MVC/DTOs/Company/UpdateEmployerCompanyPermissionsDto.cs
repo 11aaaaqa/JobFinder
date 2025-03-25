2034a55297b8b68608082b2a68bb9e9378d0d1aa
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-25 20:24:14 +0400

    Add dto

---
+++ src/Web/Web.MVC/DTOs/Company/UpdateEmployerCompanyPermissionsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.DTOs.Company
+{
+    public class UpdateEmployerCompanyPermissionsDto
+    {
+        public Guid EmployerId { get; set; }
+        public List<string> Permissions { get; set; }
+    }
+}
