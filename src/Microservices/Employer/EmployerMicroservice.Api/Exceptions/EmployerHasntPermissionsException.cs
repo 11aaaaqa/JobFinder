0e7309a72a682e54c0b86907f412d4a2fabf1b27
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-30 19:51:39 +0400

    Remove custom exception

--- src/Microservices/Employer/EmployerMicroservice.Api/Exceptions/EmployerHasntPermissionsException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EmployerMicroservice.Api.Exceptions
-{
-    public class EmployerHasntPermissionsException : Exception
-    {
-        public EmployerHasntPermissionsException() { }
-        public EmployerHasntPermissionsException(string message) : base(message) { }
-    }
-}
