cccd4ac920a758a75fac6674cd19e307b0a6b6b2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-30 19:52:43 +0400

    Add permission validator filter

---
+++ src/Web/Web.MVC/Filters/Authorization filters/Company filters/CompanyPermissionChecker.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.MVC.Filters.Authorization_filters.Company_filters
+{
+    public class CompanyPermissionChecker : TypeFilterAttribute
+    {
+        public CompanyPermissionChecker(string permission) : base(typeof(CompanyPermissionCheckerService))
+        {
+            Arguments = new object[] { permission };
+        }
+    }
+}
