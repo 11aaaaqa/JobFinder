d1b2c123970277112b1f62521c835bf24e5dae5f
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-03-31 17:16:28 +0400

    Add few more company employers permissions

--- src/Web/Web.MVC/Constants/Permissions constants/CompanyPermissionsConstants.cs
+++ src/Web/Web.MVC/Constants/Permissions constants/CompanyPermissionsConstants.cs
@@ -4,5 +4,7 @@
     {
         public const string UpdateCompanyInformationDataPermission = "Обновлять информацию о компании";
         public const string DeleteCompanyPermission = "Удалять компанию";
+        public const string ViewListOfEmployersRequestedJoiningPermission = "Просматривать список запросов на присоединение";
+        public const string AcceptRejectEmployersRequestedJoiningPermission = "Принимать и отклонять запросы на присоединение";
     }
 }
