735b5441a4f0cef8b6e1489a376dfb857a6fecde
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-01 19:01:04 +0400

    Add kafka consumer

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Kafka/Consumers/CompanyDeletedKafkaConsumer.cs
@@ -0,0 +1,77 @@
+﻿using System.Text.Json;
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using EmployerMicroservice.Api.Constants;
+using EmployerMicroservice.Api.Database;
+using EmployerMicroservice.Api.Kafka.Consumer_Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployerMicroservice.Api.Kafka.Consumers
+{
+    public class CompanyDeletedKafkaConsumer(IConfiguration configuration, IServiceScopeFactory scopeFactory) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            const string topicName = "company-deleted-topic";
+            var config = new ConsumerConfig
+            {
+                GroupId = KafkaConstants.GroupId,
+                AllowAutoCreateTopics = true,
+                BootstrapServers = configuration["Kafka:BootstrapServers"]
+            };
+            using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+            consumer.Subscribe(topicName);
+
+            using var scope = scopeFactory.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                ConsumeResult<Null, string> consumeResult = new();
+                try
+                {
+                    consumeResult = consumer.Consume(stoppingToken);
+                }
+                catch (Exception exc)
+                {
+                    if (!exc.Message.ToLower().Contains("unknown topic"))
+                        throw;
+                    using var adminClient = new AdminClientBuilder(config).Build();
+
+                    try
+                    {
+                        await adminClient.CreateTopicsAsync(new List<TopicSpecification>
+                        {
+                            new() {Name = topicName, NumPartitions = 1, ReplicationFactor = 1}
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        if (!ex.Message.ToLower().Contains("already exists"))
+                            throw;
+                    }
+                    continue;
+                }
+
+                var model = JsonSerializer.Deserialize<CompanyDeletedConsumerModel>(consumeResult.Message.Value);
+                var allCompanyEmployers = await context.Employers.Where(x => x.CompanyId == model.CompanyId)
+                    .ToListAsync(CancellationToken.None);
+                foreach (var companyEmployer in allCompanyEmployers)
+                {
+                    var employerPermissions =
+                        await context.EmployersPermissions.SingleOrDefaultAsync(x => x.EmployerId == companyEmployer.Id,
+                            CancellationToken.None);
+                    if(employerPermissions != null)
+                        context.EmployersPermissions.Remove(employerPermissions);
+
+                    companyEmployer.CompanyId = null;
+                }
+                
+                await context.SaveChangesAsync(CancellationToken.None);
+            }
+
+            consumer.Close();
+        }
+    }
+}
