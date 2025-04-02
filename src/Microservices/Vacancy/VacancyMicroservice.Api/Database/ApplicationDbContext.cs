ea11683d1a79d5d29cb019e060432153c9efec13
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-02 17:55:16 +0400

    Add database context

---
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VacancyMicroservice.Api.Models;
+
+namespace VacancyMicroservice.Api.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Vacancy> Vacancies { get; set; }
+    }
+}
