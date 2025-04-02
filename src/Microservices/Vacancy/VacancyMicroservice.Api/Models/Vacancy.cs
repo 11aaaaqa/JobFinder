96cf41a2963fe6b144260219a964696793ea5fcc
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-02 17:55:31 +0400

    Add vacancy model

---
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/Models/Vacancy.cs
@@ -0,0 +1,20 @@
+﻿namespace VacancyMicroservice.Api.Models
+{
+    public class Vacancy
+    {
+        public Guid Id { get; set; }
+        public Guid CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string Post { get; set; }
+        public int? SalaryFrom { get; set; }
+        public int? SalaryTo { get; set; }
+        public string? WorkExperience { get; set; }
+        public string EmploymentType { get; set; }
+        public bool RemoteWork { get; set; }
+        public string VacancyCities { get; set; }
+        public string Address { get; set; }
+        public string WorkerResponsibilities { get; set; }
+        public string EmployerContactPhoneNumber { get; set; }
+        public string EmployerContactEmail { get; set; }
+    }
+}
