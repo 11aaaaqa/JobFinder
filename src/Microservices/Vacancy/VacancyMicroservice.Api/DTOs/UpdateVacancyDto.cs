38f6c15c2db2a282a8ee81bf0df84b668e5dfed7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-03 19:56:55 +0400

    Add dto

---
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/DTOs/UpdateVacancyDto.cs
@@ -0,0 +1,20 @@
+﻿namespace VacancyMicroservice.Api.DTOs
+{
+    public class UpdateVacancyDto
+    {
+        public Guid Id { get; set; }
+        public string Profession { get; set; }
+        public string Position { get; set; }
+        public int? SalaryFrom { get; set; }
+        public int? SalaryTo { get; set; }
+        public string? WorkExperience { get; set; }
+        public string EmploymentType { get; set; }
+        public bool RemoteWork { get; set; }
+        public string VacancyCity { get; set; }
+        public string Address { get; set; }
+        public string? WorkerResponsibilities { get; set; }
+        public string? Description { get; set; }
+        public string EmployerContactPhoneNumber { get; set; }
+        public string EmployerContactEmail { get; set; }
+    }
+}
