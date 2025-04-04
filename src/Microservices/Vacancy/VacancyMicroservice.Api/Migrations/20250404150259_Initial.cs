916ae9506434b53d97cf0c042153dd506b79f29a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-04 19:03:58 +0400

    Add migration

---
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/Migrations/20250404150259_Initial.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VacancyMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vacancies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CompanyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CompanyName = table.Column<string>(type: "text", nullable: false),
+                    Profession = table.Column<string>(type: "text", nullable: false),
+                    Position = table.Column<string>(type: "text", nullable: false),
+                    SalaryFrom = table.Column<int>(type: "integer", nullable: true),
+                    SalaryTo = table.Column<int>(type: "integer", nullable: true),
+                    WorkExperience = table.Column<string>(type: "text", nullable: true),
+                    EmploymentType = table.Column<string>(type: "text", nullable: false),
+                    RemoteWork = table.Column<bool>(type: "boolean", nullable: false),
+                    VacancyCity = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    WorkerResponsibilities = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    EmployerContactPhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    EmployerContactEmail = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vacancies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vacancies");
+        }
+    }
+}
