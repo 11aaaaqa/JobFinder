e3cf3042a0c028ddfaef28f31135306f5cb7f10b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-04 20:30:08 +0400

    Add dto

---
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/DTOs/GetFilteredVacanciesDto.cs
@@ -0,0 +1,14 @@
+﻿namespace VacancyMicroservice.Api.DTOs
+{
+    public class GetFilteredVacanciesDto
+    {
+        public string? Profession { get; set; }
+        public string? Position { get; set; }
+        public int? SalaryFrom { get; set; }
+        public string? WorkExperience { get; set; }
+        public string? EmploymentType { get; set; }
+        public bool? RemoteWork { get; set; }
+        public List<string>? VacancyCities { get; set; }
+        public int PageNumber { get; set; }
+    }
+}
