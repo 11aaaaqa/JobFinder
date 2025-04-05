4ad96d4eb6c226139b8b830650d372d4c77a04e7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-05 19:14:57 +0400

    Add dto

---
+++ src/Web/Web.MVC/DTOs/Vacancy/AddVacancyDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Web.MVC.DTOs.Vacancy
+{
+    public class AddVacancyDto
+    {
+        public Guid Id { get; set; }
+        public Guid CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string Position { get; set; }
+        public int? SalaryFrom { get; set; }
+        public int? SalaryTo { get; set; }
+        public string? WorkExperience { get; set; }
+        public string EmploymentType { get; set; }
+        public bool RemoteWork { get; set; }
+        public string VacancyCity { get; set; }
+        public string Address { get; set; }
+        public string? WorkerResponsibilities { get; set; }
+        public string? Description { get; set; }
+        public string EmployerContactPhoneNumber { get; set; }
+        public string EmployerContactEmail { get; set; }
+    }
+}
