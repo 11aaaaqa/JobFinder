2e7744e64cc8728bb0c741c6e57ada1d61876173
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-05 19:13:55 +0400

    Register pagination service

--- src/Microservices/Vacancy/VacancyMicroservice.Api/Program.cs
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using VacancyMicroservice.Api.Database;
 using VacancyMicroservice.Api.Services;
+using VacancyMicroservice.Api.Services.Pagination;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(x => 
     x.UseNpgsql(builder.Configuration["Database:ConnectionString"]));
 
+builder.Services.AddTransient<IPaginationService, PaginationService>();
 builder.Services.AddTransient<IVacancyRepository, VacancyRepository>();
 
 builder.Services.AddControllers();
