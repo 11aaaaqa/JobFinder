05fad7711a94a6ae936ecb95ee562f38e1b4dd96
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-05 19:13:42 +0400

    Add pagination service

---
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/Services/Pagination/IPaginationService.cs
@@ -0,0 +1,12 @@
+﻿using VacancyMicroservice.Api.DTOs;
+
+namespace VacancyMicroservice.Api.Services.Pagination
+{
+    public interface IPaginationService
+    {
+        Task<bool> DoesNextAllVacanciesPageExist(int currentPageNumber);
+        Task<bool> DoesNextSearchVacanciesPageExist(string searchingQuery, int currentPageNumber);
+        Task<bool> DoesNextFilteredVacanciesPageExist(GetFilteredVacanciesDto model, int currentPageNumber);
+        Task<bool> DoesNextVacanciesByCompanyIdPageExist(Guid companyId, int currentPageNumber);
+    }
+}
---
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/Services/Pagination/PaginationService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VacancyMicroservice.Api.Constants;
+using VacancyMicroservice.Api.Database;
+using VacancyMicroservice.Api.DTOs;
+
+namespace VacancyMicroservice.Api.Services.Pagination
+{
+    public class PaginationService(ApplicationDbContext context) : IPaginationService
+    {
+        public async Task<bool> DoesNextAllVacanciesPageExist(int currentPageNumber)
+        {
+            var remainedVacanciesCount = await context.Vacancies.Skip(currentPageNumber * PaginationConstants.VacancyPageSize).CountAsync();
+            return remainedVacanciesCount > 0;
+        }
+
+        public async Task<bool> DoesNextSearchVacanciesPageExist(string searchingQuery, int currentPageNumber)
+        {
+            searchingQuery = searchingQuery.ToLower();
+            var remainedVacanciesCount = await context.Vacancies.Where(x =>
+                    x.CompanyName.ToLower().Contains(searchingQuery) |
+                    x.Position.ToLower().Contains(searchingQuery))
+                .Skip(currentPageNumber * PaginationConstants.VacancyPageSize).CountAsync();
+            return remainedVacanciesCount > 0;
+        }
+
+        public async Task<bool> DoesNextFilteredVacanciesPageExist(GetFilteredVacanciesDto model, int currentPageNumber)
+        {
+            var vacancies = context.Vacancies.AsQueryable();
+            if (model.Position is not null)
+                vacancies = vacancies.Where(x => x.Position.ToLower() == model.Position.ToLower());
+            if (model.SalaryFrom is not null)
+                vacancies = vacancies.Where(x => x.SalaryTo >= model.SalaryFrom);
+            if (model.WorkExperience is not null)
+                vacancies = vacancies.Where(x => x.WorkExperience.ToLower() == model.WorkExperience.ToLower());
+            if (model.EmploymentType is not null)
+                vacancies = vacancies.Where(x => x.EmploymentType.ToLower() == model.EmploymentType.ToLower());
+            if (model.RemoteWork is not null)
+                vacancies = vacancies.Where(x => x.RemoteWork == model.RemoteWork);
+            if (model.VacancyCities is not null)
+                vacancies = vacancies.Where(x => model.VacancyCities.Contains(x.VacancyCity));
+
+            return await vacancies.Skip(currentPageNumber * PaginationConstants.VacancyPageSize).CountAsync() > 0;
+        }
+
+        public async Task<bool> DoesNextVacanciesByCompanyIdPageExist(Guid companyId, int currentPageNumber)
+          => await context.Vacancies.Where(x => x.CompanyId == companyId)
+              .Skip(currentPageNumber * PaginationConstants.VacancyPageSize)
+              .CountAsync() > 0;
+        
+    }
+}
