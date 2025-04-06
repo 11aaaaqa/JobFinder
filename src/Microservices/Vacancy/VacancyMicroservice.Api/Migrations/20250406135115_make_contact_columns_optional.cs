8d88a0732c5db34b6d017116e84cb8c8ecab611e
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-06 17:54:55 +0400

    Add migration

---
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/Migrations/20250406135115_make_contact_columns_optional.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VacancyMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class make_contact_columns_optional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
