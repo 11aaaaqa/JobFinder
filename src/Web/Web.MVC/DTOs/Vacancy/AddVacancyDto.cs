1f1a5eb48d3cf0f1e3b2faaf5764cc8e50fd4ea1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-07 19:34:54 +0400

    Update dto

--- src/Web/Web.MVC/DTOs/Vacancy/AddVacancyDto.cs
+++ src/Web/Web.MVC/DTOs/Vacancy/AddVacancyDto.cs
@@ -1,21 +1,28 @@
-﻿namespace Web.MVC.DTOs.Vacancy
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.Vacancy
 {
     public class AddVacancyDto
     {
         public Guid Id { get; set; }
         public Guid CompanyId { get; set; }
         public string CompanyName { get; set; }
+        [Required]
         public string Position { get; set; }
         public int? SalaryFrom { get; set; }
         public int? SalaryTo { get; set; }
         public string? WorkExperience { get; set; }
         public string EmploymentType { get; set; }
         public bool RemoteWork { get; set; }
+        [Required]
         public string VacancyCity { get; set; }
+        [Required]
         public string Address { get; set; }
         public string? WorkerResponsibilities { get; set; }
         public string? Description { get; set; }
-        public string EmployerContactPhoneNumber { get; set; }
-        public string EmployerContactEmail { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string? EmployerContactPhoneNumber { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string? EmployerContactEmail { get; set; }
     }
 }
