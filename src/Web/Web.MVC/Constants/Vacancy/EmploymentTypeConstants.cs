26f5b197622d2fd19e19fb43389edf290e7b81e3
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-07 19:34:27 +0400

    Add constants

---
+++ src/Web/Web.MVC/Constants/Vacancy/EmploymentTypeConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.Constants.Vacancy
+{
+    public class EmploymentTypeConstants
+    {
+        public const string FullTimeEmploymentTypeConstant = "Полная занятость";
+        public const string PartTimeEmploymentTypeConstant = "Неполная занятость";
+    }
+}
