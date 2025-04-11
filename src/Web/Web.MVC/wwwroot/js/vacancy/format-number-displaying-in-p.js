a0eb931e66d0d285b07735a54d69d530687b4e43
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-11 21:22:52 +0400

    Add script

---
+++ src/Web/Web.MVC/wwwroot/js/vacancy/format-number-displaying-in-p.js
@@ -0,0 +1,12 @@
+﻿const paragraphs = document.querySelectorAll('p.format-number');
+
+paragraphs.forEach(p => {
+    const text = p.textContent;
+
+    const formattedText = text.replace(/(\d+)/g, (match) => {
+        const number = parseInt(match, 10);
+        return number.toLocaleString('ru-RU');
+    });
+
+    p.textContent = formattedText;
+});
