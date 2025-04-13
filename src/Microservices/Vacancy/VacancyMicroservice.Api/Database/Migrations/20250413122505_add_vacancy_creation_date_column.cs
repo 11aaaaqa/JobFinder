4347024822f93d145b9a280567c4bced28ea9473
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-13 16:37:09 +0400

    Add migration

---
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/Database/Migrations/20250413122505_add_vacancy_creation_date_column.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VacancyMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_vacancy_creation_date_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "CreatedAt",
+                table: "Vacancies",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Vacancies");
+        }
+    }
+}
