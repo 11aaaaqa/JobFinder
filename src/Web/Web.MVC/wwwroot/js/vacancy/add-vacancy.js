ff989dc08285c2c4344d0736a9a468c379265a79
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-13 19:05:37 +0400

    Update script

--- src/Web/Web.MVC/wwwroot/js/vacancy/add-vacancy.js
+++ src/Web/Web.MVC/wwwroot/js/vacancy/add-vacancy.js
@@ -1,6 +1,17 @@
-﻿function updateWorkerResponsibilitiesTextarea() {
+﻿function execCmd(command) {
+    document.execCommand(command, false, null);
+    updateButtonStates();
+}
+
+function updateButtonStates() {
+    document.getElementById('boldBtn').classList.toggle('active', document.queryCommandState('bold'));
+    document.getElementById('italicBtn').classList.toggle('active', document.queryCommandState('italic'));
+}
+
+function updateWorkerResponsibilitiesTextarea() {
     const content = document.getElementById('worker-responsibilities-input').innerHTML;
     document.getElementById('worker-responsibilities-hidden-textarea').value = content;
+    updateButtonStates();
 }
 
 const inputDiv = document.getElementById('worker-responsibilities-input');
@@ -9,3 +20,7 @@ inputDiv.addEventListener('input', () => {
     inputDiv.style.height = 'auto';
     inputDiv.style.height = `${inputDiv.scrollHeight}px`;
 });
+
+window.onload = function () {
+    updateButtonStates();
+};
