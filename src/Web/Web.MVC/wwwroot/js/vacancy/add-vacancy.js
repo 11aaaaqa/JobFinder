1f72941f3dbff0340419b5e912047d9ce5df8b6a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-13 17:10:54 +0400

    Add script

---
+++ src/Web/Web.MVC/wwwroot/js/vacancy/add-vacancy.js
@@ -0,0 +1,11 @@
+﻿function updateWorkerResponsibilitiesTextarea() {
+    const content = document.getElementById('worker-responsibilities-input').innerHTML;
+    document.getElementById('worker-responsibilities-hidden-textarea').value = content;
+}
+
+const inputDiv = document.getElementById('worker-responsibilities-input');
+
+inputDiv.addEventListener('input', () => {
+    inputDiv.style.height = 'auto';
+    inputDiv.style.height = `${inputDiv.scrollHeight}px`;
+});
