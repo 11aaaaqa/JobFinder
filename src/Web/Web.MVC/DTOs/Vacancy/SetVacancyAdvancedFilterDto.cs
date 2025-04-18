d4bbf09a7b9843f34a4d167b1e24ad929b2607cf
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-18 19:41:55 +0400

    Add dto

---
+++ src/Web/Web.MVC/DTOs/Vacancy/SetVacancyAdvancedFilterDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.MVC.DTOs.Vacancy
+{
+    public class SetVacancyAdvancedFilterDto
+    {
+        public string? Position { get; set; }
+        public int? SalaryFrom { get; set; }
+        public string? WorkExperience { get; set; }
+        public string? EmploymentType { get; set; }
+        public bool OfficeWorkType { get; set; }
+        public bool RemoteWorkType { get; set; }
+        public List<string>? VacancyCities { get; set; }
+    }
+}
