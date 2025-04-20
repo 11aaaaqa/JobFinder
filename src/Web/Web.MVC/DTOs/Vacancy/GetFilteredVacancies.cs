0a9a9ac079099535b1720da75f98b82ef9b2df80
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-20 12:25:29 +0400

    Add model

---
+++ src/Web/Web.MVC/DTOs/Vacancy/GetFilteredVacancies.cs
@@ -0,0 +1,12 @@
+﻿namespace Web.MVC.DTOs.Vacancy
+{
+    public class GetFilteredVacancies
+    {
+        public string? Position { get; set; }
+        public int? SalaryFrom { get; set; }
+        public string? WorkExperience { get; set; }
+        public string? EmploymentType { get; set; }
+        public bool? RemoteWork { get; set; }
+        public List<string>? VacancyCities { get; set; }
+    }
+}
