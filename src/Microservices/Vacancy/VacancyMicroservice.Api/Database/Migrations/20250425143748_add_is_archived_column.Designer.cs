cf0ccf131d9c7f9e087e0701b257f50f8f9706c0
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-25 18:43:40 +0400

    Add migration

--- src/Microservices/Vacancy/VacancyMicroservice.Api/Database/Migrations/20250425143748_add_is_archived_column.Designer.cs
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/Database/Migrations/20250425143748_add_is_archived_column.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VacancyMicroservice.Api.Database;
@@ -11,9 +12,11 @@ using VacancyMicroservice.Api.Database;
 namespace VacancyMicroservice.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250425143748_add_is_archived_column")]
+    partial class add_is_archived_column
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/Database/Migrations/20250425143748_add_is_archived_column.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VacancyMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_is_archived_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsArchived",
+                table: "Vacancies",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsArchived",
+                table: "Vacancies");
+        }
+    }
+}
