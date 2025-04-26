c27af1d36b51731326b81d58604f8086c48d5c2b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-04-26 20:44:22 +0400

    Add script

---
+++ src/Web/Web.MVC/wwwroot/js/vacancy/edit-vacancy.js
@@ -0,0 +1,26 @@
+﻿function execCmd(command) {
+    document.execCommand(command, false, null);
+    updateButtonStates();
+}
+
+function updateButtonStates() {
+    document.getElementById('boldBtn').classList.toggle('active', document.queryCommandState('bold'));
+    document.getElementById('italicBtn').classList.toggle('active', document.queryCommandState('italic'));
+}
+
+function updateWorkerResponsibilitiesTextarea() {
+    const content = document.getElementById('worker-responsibilities-input').innerHTML;
+    document.getElementById('worker-responsibilities-hidden-textarea').value = content;
+    updateButtonStates();
+}
+
+const inputDiv = document.getElementById('worker-responsibilities-input');
+
+inputDiv.addEventListener('input', () => {
+    inputDiv.style.height = 'auto';
+    inputDiv.style.height = `${inputDiv.scrollHeight}px`;
+});
+
+window.onload = function () {
+    updateButtonStates();
+};
