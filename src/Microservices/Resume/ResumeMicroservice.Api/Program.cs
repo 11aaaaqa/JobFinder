1a0cddf2ed749230644b6663eea4896a1ab60f4a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-01 10:34:59 +0400

    Register database context

--- src/Microservices/Resume/ResumeMicroservice.Api/Program.cs
+++ src/Microservices/Resume/ResumeMicroservice.Api/Program.cs
@@ -1,12 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ResumeMicroservice.Api.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(x => 
+    x.UseNpgsql(builder.Configuration["Database:ConnectionString"]));
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.UseAuthorization();
 
