a1f2488d50393952f4091fe73eae14411637355e
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-01 10:34:18 +0400

    Update model

--- src/Microservices/Resume/ResumeMicroservice.Api/Models/Resume.cs
+++ src/Microservices/Resume/ResumeMicroservice.Api/Models/Resume.cs
@@ -14,12 +14,9 @@ namespace ResumeMicroservice.Api.Models
         public DateOnly? DateOfBirth { get; set; }
         public string? City { get; set; }
         public string Status { get; set; }
-        public string Specialization { get; set; }
         public bool ReadyToMove { get; set; }
         public string? PhoneNumber { get; set; }
         public string? AboutMe { get; set; }
-        public uint? DesiredSalaryFrom { get; set; }
-        public uint? DesiredSalaryTo { get; set; }
         public uint? DesiredSalary { get; set; }
         public List<Education> Educations { get; set; }
         public List<EmployeeExperience> EmployeeExperience { get; set; }
