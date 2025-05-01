9fe3fec16fde3b451e9eff7b84199e4fef298f63
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-01 17:02:52 +0400

    Add DTOs

---
+++ src/Microservices/Resume/ResumeMicroservice.Api/DTOs/UpdateResumeDto.cs
@@ -0,0 +1,27 @@
+﻿using ResumeMicroservice.Api.Models.Skills;
+
+namespace ResumeMicroservice.Api.DTOs
+{
+    public class UpdateResumeDto
+    {
+        public Guid Id { get; set; }
+        public string ResumeTitle { get; set; }
+        public List<string>? OccupationTypes { get; set; }
+        public List<string>? WorkTypes { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string? Patronymic { get; set; }
+        public string? Gender { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
+        public string? City { get; set; }
+        public bool ReadyToMove { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? AboutMe { get; set; }
+        public uint? DesiredSalary { get; set; }
+        public TimeSpan WorkingExperience { get; set; }
+        public List<Education>? Educations { get; set; }
+        public List<EmployeeExperience>? EmployeeExperience { get; set; }
+        public List<ForeignLanguage>? ForeignLanguages { get; set; }
+    }
+}
