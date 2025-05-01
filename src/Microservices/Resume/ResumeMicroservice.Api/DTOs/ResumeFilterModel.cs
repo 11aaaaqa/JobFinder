ab45bd0174bab2ff69794bec62cfaf4a1972af09
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-01 17:02:52 +0400

    Add DTOs

---
+++ src/Microservices/Resume/ResumeMicroservice.Api/DTOs/ResumeFilterModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ResumeMicroservice.Api.DTOs
+{
+    public class ResumeFilterModel
+    {
+        public string? ResumeTitle { get; set; }
+        public List<string>? OccupationTypes { get; set; }
+        public List<string>? WorkTypes { get; set; }
+        public List<string>? Cities { get; set; }
+        public uint? DesiredSalaryTo { get; set; }
+        public TimeSpan? WorkingExperienceFrom { get; set; }
+    }
+}
