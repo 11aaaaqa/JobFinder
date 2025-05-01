7d0ffa7e2f7963b27267adfa81fd41e06acd878c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-01 17:03:53 +0400

    Register dependencies

--- src/Microservices/Resume/ResumeMicroservice.Api/Program.cs
+++ src/Microservices/Resume/ResumeMicroservice.Api/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using ResumeMicroservice.Api.Database;
+using ResumeMicroservice.Api.Services.Pagination;
+using ResumeMicroservice.Api.Services.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(x => 
     x.UseNpgsql(builder.Configuration["Database:ConnectionString"]));
+
+builder.Services.AddTransient<IResumeRepository, ResumeRepository>();
+builder.Services.AddTransient<ICheckForNextPageExistingService, CheckForNextPageExistingService>();
 
 builder.Services.AddControllers();
 
