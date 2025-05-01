fa0e2881ec9028bc6b4a3950311b89959adc56ce
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-01 10:39:03 +0400

    Add migration

---
+++ src/Microservices/Resume/ResumeMicroservice.Api/Database/Migrations/20250501063749_Initial.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResumeMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Resumes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EmployeeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ResumeTitle = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Surname = table.Column<string>(type: "text", nullable: false),
+                    Patronymic = table.Column<string>(type: "text", nullable: true),
+                    Gender = table.Column<string>(type: "text", nullable: true),
+                    DateOfBirth = table.Column<DateOnly>(type: "date", nullable: true),
+                    City = table.Column<string>(type: "text", nullable: true),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    ReadyToMove = table.Column<bool>(type: "boolean", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    AboutMe = table.Column<string>(type: "text", nullable: true),
+                    DesiredSalary = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resumes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Education",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EducationType = table.Column<string>(type: "text", nullable: false),
+                    EducationForm = table.Column<string>(type: "text", nullable: true),
+                    EducationalInstitution = table.Column<string>(type: "text", nullable: true),
+                    Specialization = table.Column<string>(type: "text", nullable: true),
+                    ResumeId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Education", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Education_Resumes_ResumeId",
+                        column: x => x.ResumeId,
+                        principalTable: "Resumes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeExperience",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CompanyPost = table.Column<string>(type: "text", nullable: false),
+                    CompanyName = table.Column<string>(type: "text", nullable: false),
+                    WorkingFromMonth = table.Column<string>(type: "text", nullable: false),
+                    WorkingUntilMonth = table.Column<string>(type: "text", nullable: false),
+                    WorkingFromYear = table.Column<string>(type: "text", nullable: false),
+                    WorkingUntilYear = table.Column<string>(type: "text", nullable: false),
+                    CurrentlyWorkHere = table.Column<bool>(type: "boolean", nullable: false),
+                    Responsibilities = table.Column<string>(type: "text", nullable: true),
+                    ResumeId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeExperience", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeExperience_Resumes_ResumeId",
+                        column: x => x.ResumeId,
+                        principalTable: "Resumes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ForeignLanguage",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    LanguageName = table.Column<string>(type: "text", nullable: false),
+                    LanguageProficiencyLevel = table.Column<string>(type: "text", nullable: false),
+                    ResumeId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForeignLanguage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ForeignLanguage_Resumes_ResumeId",
+                        column: x => x.ResumeId,
+                        principalTable: "Resumes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Education_ResumeId",
+                table: "Education",
+                column: "ResumeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeExperience_ResumeId",
+                table: "EmployeeExperience",
+                column: "ResumeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForeignLanguage_ResumeId",
+                table: "ForeignLanguage",
+                column: "ResumeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Education");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeExperience");
+
+            migrationBuilder.DropTable(
+                name: "ForeignLanguage");
+
+            migrationBuilder.DropTable(
+                name: "Resumes");
+        }
+    }
+}
