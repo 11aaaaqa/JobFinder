10078e9ee82054e22c24e02012a9bd3f5f170095
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-01 17:03:42 +0400

    Add pagination checking for next page existing service

---
+++ src/Microservices/Resume/ResumeMicroservice.Api/Services/Pagination/ICheckForNextPageExistingService.cs
@@ -0,0 +1,11 @@
+﻿using ResumeMicroservice.Api.DTOs;
+
+namespace ResumeMicroservice.Api.Services.Pagination
+{
+    public interface ICheckForNextPageExistingService
+    {
+        Task<bool> DoesNextAllResumesPageExistAsync(string? searchingQuery, int currentPageNumber);
+        Task<bool> DoesNextResumesWithActiveStatusPageExistAsync(string? searchingQuery, int currentPageNumber);
+        Task<bool> DoesNextFilteredResumesPageExistAsync(ResumeFilterModel model, string? searchingQuery, int currentPageNumber);
+    }
+}
