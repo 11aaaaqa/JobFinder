44d5b5684fe251902ff7f38a16bea0b70a6b8cc7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-02 16:12:12 +0400

    Add DTOs

---
+++ src/Web/Web.MVC/DTOs/Resume/EmployeeExperienceDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.MVC.DTOs.Resume
+{
+    public class EmployeeExperienceDto
+    {
+        public Guid Id { get; set; }
+        public string CompanyPost { get; set; }
+        public string CompanyName { get; set; }
+        public string WorkingFrom { get; set; }
+        public string WorkingUntil { get; set; }
+        public TimeSpan WorkingDuration { get; set; } = TimeSpan.Zero;
+        public string? Responsibilities { get; set; }
+    }
+}
---
+++ src/Web/Web.MVC/DTOs/Resume/ForeignLanguageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.MVC.DTOs.Resume
+{
+    public class ForeignLanguageDto
+    {
+        public Guid Id { get; set; }
+        public string LanguageName { get; set; }
+        public string LanguageProficiencyLevel { get; set; }
+    }
+}
