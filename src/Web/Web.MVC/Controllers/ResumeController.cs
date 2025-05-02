dd6a8814614a277006f8b8bc70cd3bff40eec2b6
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-02 11:42:58 +0400

    Update getting my resumes method

--- src/Web/Web.MVC/Controllers/ResumeController.cs
+++ src/Web/Web.MVC/Controllers/ResumeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Web.MVC.Models.ApiResponses;
+using Web.MVC.Models.ApiResponses.Resume;
 
 namespace Web.MVC.Controllers
 {
@@ -20,12 +21,19 @@ namespace Web.MVC.Controllers
         public async Task<IActionResult> GetMyResumes()
         {
             using HttpClient httpClient = httpClientFactory.CreateClient();
-            var response = await httpClient.GetAsync($"{url}/api/Employee/GetEmployeeByEmail?email={User.Identity.Name}");
-            response.EnsureSuccessStatusCode();
+            var employeeResponse = await httpClient.GetAsync($"{url}/api/Employee/GetEmployeeByEmail?email={User.Identity.Name}");
+            employeeResponse.EnsureSuccessStatusCode();
 
-            var employee = await response.Content.ReadFromJsonAsync<EmployeeResponse>();
+            var employee = await employeeResponse.Content.ReadFromJsonAsync<EmployeeResponse>();
 
-            return View(employee);
+            var resumesResponse = await httpClient.GetAsync($"{url}/api/Resume/GetResumesByEmployeeId/{employee.Id}");
+            resumesResponse.EnsureSuccessStatusCode();
+
+            var resumes = await resumesResponse.Content.ReadFromJsonAsync<List<ResumeResponse>>();
+
+            ViewBag.Employee = employee;
+
+            return View(resumes);
         }
     }
 }
