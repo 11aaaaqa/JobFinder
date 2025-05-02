224b120f239f72c9bd74405e5a9cfcd50d6813b0
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-02 16:13:11 +0400

    Add migration

--- src/Microservices/Resume/ResumeMicroservice.Api/Database/Migrations/20250502115120_remove_education_form_and_currently_work_here_columns.Designer.cs
+++ src/Microservices/Resume/ResumeMicroservice.Api/Database/Migrations/20250502115120_remove_education_form_and_currently_work_here_columns.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ResumeMicroservice.Api.Database;
@@ -12,9 +13,11 @@ using ResumeMicroservice.Api.Database;
 namespace ResumeMicroservice.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250502115120_remove_education_form_and_currently_work_here_columns")]
+    partial class remove_education_form_and_currently_work_here_columns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
