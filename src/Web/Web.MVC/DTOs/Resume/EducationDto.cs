b7821e6f83018da4beccb8f40ca6b6edecbe8e8b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-02 16:12:12 +0400

    Add DTOs

---
+++ src/Web/Web.MVC/DTOs/Resume/EducationDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.Resume
+{
+    public class EducationDto
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
+        [Display(Name = "Уровень образования")]
+        public string EducationType { get; set; }
+        [Display(Name = "Образовательное учреждение")]
+        public string? EducationalInstitution { get; set; }
+        [Display(Name = "Специализация")]
+        public string? Specialization { get; set; }
+    }
+}
