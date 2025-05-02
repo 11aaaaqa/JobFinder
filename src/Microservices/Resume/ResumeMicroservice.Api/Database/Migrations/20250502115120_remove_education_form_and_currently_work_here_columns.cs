a55bf2c8a77fcf0f7d83401b2df7ac7fba6a7af4
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-02 16:13:11 +0400

    Add migration

---
+++ src/Microservices/Resume/ResumeMicroservice.Api/Database/Migrations/20250502115120_remove_education_form_and_currently_work_here_columns.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResumeMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_education_form_and_currently_work_here_columns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentlyWorkHere",
+                table: "EmployeeExperience");
+
+            migrationBuilder.DropColumn(
+                name: "EducationForm",
+                table: "Education");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CurrentlyWorkHere",
+                table: "EmployeeExperience",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EducationForm",
+                table: "Education",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
