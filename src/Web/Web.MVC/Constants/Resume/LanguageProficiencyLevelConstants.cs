0cd3151e74de2b67759a46d659a4d5b7dcf924a1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-02 18:37:32 +0400

    Add constants

---
+++ src/Web/Web.MVC/Constants/Resume/LanguageProficiencyLevelConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Web.MVC.Constants.Resume
+{
+    public class LanguageProficiencyLevelConstants
+    {
+        public const string A1 = "A1 - Beginner";
+        public const string A2 = "A2 - Elementary";
+        public const string B1 = "B1 - Intermediate";
+        public const string B2 = "B2 - Upper intermediate";
+        public const string C1 = "C1 - Advanced";
+        public const string C2 = "C2 - Proficient";
+    }
+}
