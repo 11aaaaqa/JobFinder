d118f04a4f447dff2ef2d7ffec5ef7934ac62ecc
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-02 16:13:18 +0400

    Update response models

--- src/Web/Web.MVC/Models/ApiResponses/Resume/EducationResponse.cs
+++ src/Web/Web.MVC/Models/ApiResponses/Resume/EducationResponse.cs
@@ -4,7 +4,6 @@
     {
         public Guid Id { get; set; }
         public string EducationType { get; set; }
-        public string? EducationForm { get; set; }
         public string? EducationalInstitution { get; set; }
         public string? Specialization { get; set; }
     }
