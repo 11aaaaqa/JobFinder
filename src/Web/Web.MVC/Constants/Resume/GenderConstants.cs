d6703d0aad9a5ae7fab1e5783581ffb3042a8742
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-02 16:11:51 +0400

    Add constants

---
+++ src/Web/Web.MVC/Constants/Resume/GenderConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.Constants.Resume
+{
+    public class GenderConstants
+    {
+        public const string Male = "Мужской";
+        public const string Female = "Женский";
+    }
+}
