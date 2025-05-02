ce4d671a792e1f5b1d4d16fd6bf741ee92a90bce
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-02 11:49:37 +0400

    Add dto

---
+++ src/Web/Web.MVC/DTOs/Resume/AddResumeDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Web.MVC.Models.ApiResponses.Resume;
+
+namespace Web.MVC.DTOs.Resume
+{
+    public class AddResumeDto
+    {
+        public Guid Id { get; set; }
+        public Guid EmployeeId { get; set; }
+        [Required]
+        public string ResumeTitle { get; set; }
+        public List<string>? OccupationTypes { get; set; }
+        public List<string>? WorkTypes { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        public string? Patronymic { get; set; }
+        public string? Gender { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
+        public string? City { get; set; }
+        public string Status { get; set; }
+        public bool ReadyToMove { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? AboutMe { get; set; }
+        public uint? DesiredSalary { get; set; }
+        public List<EducationResponse>? Educations { get; set; }
+        public List<EmployeeExperienceResponse>? EmployeeExperience { get; set; }
+        public List<ForeignLanguageResponse>? ForeignLanguages { get; set; }
+    }
+}
