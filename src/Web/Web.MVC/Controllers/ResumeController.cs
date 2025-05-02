1d4b279b5d74f53c7976d4682571e4ba2c12455c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-02 11:49:47 +0400

    Add creating resume method

--- src/Web/Web.MVC/Controllers/ResumeController.cs
+++ src/Web/Web.MVC/Controllers/ResumeController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Web.MVC.DTOs.Resume;
 using Web.MVC.Models.ApiResponses;
 using Web.MVC.Models.ApiResponses.Resume;
 
@@ -34,6 +37,39 @@ namespace Web.MVC.Controllers
             ViewBag.Employee = employee;
 
             return View(resumes);
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("employee/profile/resumes/create")]
+        public async Task<IActionResult> AddResume()
+        {
+            using HttpClient httpClient = httpClientFactory.CreateClient();
+
+            var response = await httpClient.GetAsync($"{url}/api/Employee/GetEmployeeByEmail?{User.Identity.Name}");
+            response.EnsureSuccessStatusCode();
+
+            var employee = await response.Content.ReadFromJsonAsync<EmployeeResponse>();
+
+            return View(new AddResumeDto{Status = employee.Status});
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("employee/profile/resumes/create")]
+        public async Task<IActionResult> AddResume(AddResumeDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                using HttpClient httpClient = httpClientFactory.CreateClient();
+                using StringContent jsonContent = new(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json");
+
+                var response = await httpClient.PostAsync($"{url}/api/Resume/AddResume", jsonContent);
+                response.EnsureSuccessStatusCode();
+
+                return RedirectToAction("GetMyResumes"); // change to redirection to created resume
+            }
+            return View();
         }
     }
 }
