83105da5e36362cd63c84fce0f6237abf301f48b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-02 11:42:49 +0400

    Add api response models

---
+++ src/Web/Web.MVC/Models/ApiResponses/Resume/EducationResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.MVC.Models.ApiResponses.Resume
+{
+    public class EducationResponse
+    {
+        public Guid Id { get; set; }
+        public string EducationType { get; set; }
+        public string? EducationForm { get; set; }
+        public string? EducationalInstitution { get; set; }
+        public string? Specialization { get; set; }
+    }
+}
---
+++ src/Web/Web.MVC/Models/ApiResponses/Resume/EmployeeExperienceResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.MVC.Models.ApiResponses.Resume
+{
+    public class EmployeeExperienceResponse
+    {
+        public Guid Id { get; set; }
+        public string CompanyPost { get; set; }
+        public string CompanyName { get; set; }
+        public string WorkingFrom { get; set; }
+        public string WorkingUntil { get; set; }
+        public TimeSpan WorkingDuration { get; set; }
+        public bool CurrentlyWorkHere { get; set; }
+        public string? Responsibilities { get; set; }
+    }
+}
