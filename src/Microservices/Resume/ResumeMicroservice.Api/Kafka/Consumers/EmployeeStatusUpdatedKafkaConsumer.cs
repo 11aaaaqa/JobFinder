63760a94ed6a15605073fc49396960925c0f40c2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-04 13:41:29 +0400

    Add kafka consumer

---
+++ src/Microservices/Resume/ResumeMicroservice.Api/Kafka/Consumers/EmployeeStatusUpdatedKafkaConsumer.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.Json;
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using Microsoft.EntityFrameworkCore;
+using ResumeMicroservice.Api.Constants;
+using ResumeMicroservice.Api.Database;
+using ResumeMicroservice.Api.Kafka.Consumer_models;
+
+namespace ResumeMicroservice.Api.Kafka.Consumers
+{
+    public class EmployeeStatusUpdatedKafkaConsumer(IConfiguration configuration,
+        IServiceScopeFactory scopeFactory) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            const string topicName = "employee-status-updated-topic";
+            var config = new ConsumerConfig
+            {
+                GroupId = KafkaConstants.GroupId,
+                AllowAutoCreateTopics = true,
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                BootstrapServers = configuration["Kafka:BootstrapServers"]
+            };
+            using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+            
+            consumer.Subscribe(topicName);
+
+            using var scope = scopeFactory.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                ConsumeResult<Null, string> consumeResult = new();
+                try
+                {
+                    consumeResult = consumer.Consume(stoppingToken);
+                }
+                catch (Exception exc)
+                {
+                    if (!exc.Message.ToLower().Contains("unknown topic"))
+                        throw;
+                    using var adminClient = new AdminClientBuilder(config).Build();
+
+                    try
+                    {
+                        await adminClient.CreateTopicsAsync(new List<TopicSpecification>
+                        {
+                            new() {Name = topicName, NumPartitions = 1, ReplicationFactor = 1}
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        if (!ex.Message.ToLower().Contains("already exists"))
+                            throw;
+                    }
+                    continue;
+                }
+
+                var model = JsonSerializer.Deserialize<EmployeeStatusUpdatedConsumerModel>(consumeResult.Message.Value);
+                var resumes = await context.Resumes.Where(x => x.EmployeeId == model.EmployeeId)
+                    .ToListAsync(CancellationToken.None);
+                foreach (var resume in resumes)
+                {
+                    resume.Status = model.Status;
+                }
+
+                await context.SaveChangesAsync(CancellationToken.None);
+            }
+
+            consumer.Close();
+        }
+    }
+}
