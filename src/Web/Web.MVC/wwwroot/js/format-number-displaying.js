5cbe337e6725b54bea4f623adea49c30858684e8
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-09 18:40:57 +0400

    Add script

---
+++ src/Web/Web.MVC/wwwroot/js/format-number-displaying.js
@@ -0,0 +1,12 @@
+﻿const elements = document.querySelectorAll('.format-number');
+
+elements.forEach(el => {
+    const text = el.textContent;
+
+    const formattedText = text.replace(/(\d+)/g, (match) => {
+        const number = parseInt(match, 10);
+        return number.toLocaleString('ru-RU');
+    });
+
+    el.textContent = formattedText;
+});
