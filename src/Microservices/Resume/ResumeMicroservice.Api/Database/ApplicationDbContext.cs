81990bce74b94318dcddd3ae2a18ce21f190d55c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-10 16:47:13 +0400

    Add setting up connection between resume table and its connections

--- src/Microservices/Resume/ResumeMicroservice.Api/Database/ApplicationDbContext.cs
+++ src/Microservices/Resume/ResumeMicroservice.Api/Database/ApplicationDbContext.cs
@@ -8,5 +8,26 @@ namespace ResumeMicroservice.Api.Database
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Resume> Resumes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Resume>()
+                .HasMany(x => x.EmployeeExperience)
+                .WithOne()
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Resume>()
+                .HasMany(x => x.ForeignLanguages)
+                .WithOne()
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Resume>()
+                .HasMany(x => x.Educations)
+                .WithOne()
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
