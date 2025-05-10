3304bccec965fbca89b594a7e52b844b2250032a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-10 16:47:19 +0400

    Add migration

--- src/Microservices/Resume/ResumeMicroservice.Api/Database/Migrations/20250510123735_add_connection_between_resume_model_and_children.Designer.cs
+++ src/Microservices/Resume/ResumeMicroservice.Api/Database/Migrations/20250510123735_add_connection_between_resume_model_and_children.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ResumeMicroservice.Api.Database;
@@ -12,9 +13,11 @@ using ResumeMicroservice.Api.Database;
 namespace ResumeMicroservice.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250510123735_add_connection_between_resume_model_and_children")]
+    partial class add_connection_between_resume_model_and_children
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
