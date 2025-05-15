4893e101d2e592cb172bb1a539f02516683041ad
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-15 17:39:32 +0400

    Add ef core resume's dependency children tables

--- src/Microservices/Resume/ResumeMicroservice.Api/Database/ApplicationDbContext.cs
+++ src/Microservices/Resume/ResumeMicroservice.Api/Database/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ResumeMicroservice.Api.Models;
+using ResumeMicroservice.Api.Models.Skills;
 
 namespace ResumeMicroservice.Api.Database
 {
@@ -8,24 +9,30 @@ namespace ResumeMicroservice.Api.Database
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Resume> Resumes { get; set; }
+        public DbSet<Education> Education { get; set; }
+        public DbSet<ForeignLanguage> ForeignLanguage { get; set; }
+        public DbSet<EmployeeExperience> EmployeeExperience { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Resume>()
                 .HasMany(x => x.EmployeeExperience)
                 .WithOne()
+                .HasForeignKey(x => x.ResumeId)
                 .IsRequired()
                 .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<Resume>()
                 .HasMany(x => x.ForeignLanguages)
                 .WithOne()
+                .HasForeignKey(x => x.ResumeId)
                 .IsRequired()
                 .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<Resume>()
                 .HasMany(x => x.Educations)
                 .WithOne()
+                .HasForeignKey(x => x.ResumeId)
                 .IsRequired()
                 .OnDelete(DeleteBehavior.Cascade);
         }
