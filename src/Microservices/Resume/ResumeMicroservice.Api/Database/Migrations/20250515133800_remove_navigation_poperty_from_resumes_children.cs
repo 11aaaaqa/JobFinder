e13e077d8ca7da8d66c95c5d5c6c61476edcf955
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-15 17:38:27 +0400

    Add migrations

---
+++ src/Microservices/Resume/ResumeMicroservice.Api/Database/Migrations/20250515133800_remove_navigation_poperty_from_resumes_children.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResumeMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_navigation_poperty_from_resumes_children : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
