ca6ca24ea603af38dfa60e815ff7bb8829af4f64
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-18 19:36:09 +0400

    Add constants

---
+++ src/Microservices/Resume/ResumeMicroservice.Api/Constants/WorkExperienceConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace ResumeMicroservice.Api.Constants
+{
+    public class WorkExperienceConstants
+    {
+        public const string NoExperience = "Без опыта";
+        public const string LessThanOneYear = "Менее года";
+        public const string FromOneToThreeYears = "От 1 года до 3 лет";
+        public const string FromThreeToSixYears = "От 3 до 6 лет";
+        public const string MoreThanSixYears = "Более 6 лет";
+    }
+}
