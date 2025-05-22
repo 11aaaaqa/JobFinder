99828e5c08425321e22c70583ecda3da33054859
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-22 17:04:46 +0400

    Add model

---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Models/FavoriteVacancy.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookmarkMicroservice.Api.Models
+{
+    public class FavoriteVacancy
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid EmployeeId { get; set; }
+        public Guid VacancyId { get; set; }
+        public string Position { get; set; }
+        public int? SalaryFrom { get; set; }
+        public int? SalaryTo { get; set; }
+        public string? WorkExperience { get; set; }
+        public string CompanyName { get; set; }
+        public string VacancyCity { get; set; }
+    }
+}
