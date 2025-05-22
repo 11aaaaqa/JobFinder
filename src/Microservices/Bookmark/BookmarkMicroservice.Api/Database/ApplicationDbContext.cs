16f9ba1c45d4963854e0f4dfe610f17894af0661
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-22 16:58:20 +0400

    Add database context

---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BookmarkMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookmarkMicroservice.Api.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<FavoriteVacancy> FavoriteVacancies { get; set; }
+    }
+}
