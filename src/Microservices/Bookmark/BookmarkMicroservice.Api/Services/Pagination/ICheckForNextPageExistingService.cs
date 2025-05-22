01c00dc521b41ba891f9da110339058465c1e64e
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-22 17:04:56 +0400

    Add pagination service

---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Services/Pagination/ICheckForNextPageExistingService.cs
@@ -0,0 +1,7 @@
+﻿namespace BookmarkMicroservice.Api.Services.Pagination
+{
+    public interface ICheckForNextPageExistingService
+    {
+        Task<bool> DoesNextFavoriteVacanciesByEmployeeIdPageExistAsync(Guid employeeId, string? searchingQuery, int currentPageNumber);
+    }
+}
