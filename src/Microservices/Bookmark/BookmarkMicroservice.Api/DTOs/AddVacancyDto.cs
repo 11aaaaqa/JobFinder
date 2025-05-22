e254241cd533581dcbcb3fe258872eeb418f4498
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-22 16:58:24 +0400

    Add dto

---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/DTOs/AddVacancyDto.cs
@@ -0,0 +1,14 @@
+﻿namespace BookmarkMicroservice.Api.DTOs
+{
+    public class AddVacancyDto
+    {
+        public Guid EmployeeId { get; set; }
+        public Guid VacancyId { get; set; }
+        public string Position { get; set; }
+        public int? SalaryFrom { get; set; }
+        public int? SalaryTo { get; set; }
+        public string? WorkExperience { get; set; }
+        public string CompanyName { get; set; }
+        public string VacancyCity { get; set; }
+    }
+}
