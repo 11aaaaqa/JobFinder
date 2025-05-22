bda042382625fa92b4a549636ce6e59336b61672
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-22 17:05:11 +0400

    Add favorite vacancy service

---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Services/Repositories/IFavoriteVacancyRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookmarkMicroservice.Api.DTOs;
+using BookmarkMicroservice.Api.Models;
+
+namespace BookmarkMicroservice.Api.Services.Repositories
+{
+    public interface IFavoriteVacancyRepository
+    {
+        Task<List<FavoriteVacancy>> GetFavoriteVacanciesByEmployeeIdAsync(Guid employeeId, int pageNumber, string? searchingQuery);
+        Task AddToFavoritesAsync(AddVacancyDto model);
+        Task DeleteFromFavoritesAsync(Guid vacancyId, Guid employeeId);
+        Task<bool> IsVacancyInFavouritesAsync(Guid vacancyId, Guid employeeId);
+    }
+}
