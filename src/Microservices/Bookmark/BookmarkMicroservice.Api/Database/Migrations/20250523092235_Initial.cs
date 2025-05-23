30f712ccf182999a441910d85582e0e029e18c4d
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-23 13:23:52 +0400

    Add migration

---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Database/Migrations/20250523092235_Initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookmarkMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavoriteVacancies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EmployeeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    VacancyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Position = table.Column<string>(type: "text", nullable: false),
+                    SalaryFrom = table.Column<int>(type: "integer", nullable: true),
+                    SalaryTo = table.Column<int>(type: "integer", nullable: true),
+                    WorkExperience = table.Column<string>(type: "text", nullable: true),
+                    CompanyName = table.Column<string>(type: "text", nullable: false),
+                    VacancyCity = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteVacancies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteVacancies");
+        }
+    }
+}
