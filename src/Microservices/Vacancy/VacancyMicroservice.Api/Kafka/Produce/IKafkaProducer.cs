16edba157a33cf39890be13f36bab7a5db618e50
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-23 13:53:58 +0400

    Add kafka producer

---
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/Kafka/Produce/IKafkaProducer.cs
@@ -0,0 +1,9 @@
+﻿using Confluent.Kafka;
+
+namespace VacancyMicroservice.Api.Kafka.Produce
+{
+    public interface IKafkaProducer
+    {
+        Task ProduceAsync(string topic, Message<Null, string> message);
+    }
+}
---
+++ src/Microservices/Vacancy/VacancyMicroservice.Api/Kafka/Produce/KafkaProducer.cs
@@ -0,0 +1,22 @@
+﻿using Confluent.Kafka;
+
+namespace VacancyMicroservice.Api.Kafka.Produce
+{
+    public class KafkaProducer(IConfiguration configuration) : IKafkaProducer
+    {
+        public async Task ProduceAsync(string topic, Message<Null, string> message)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = configuration["Kafka:BootstrapServers"],
+                AllowAutoCreateTopics = true,
+                Acks = Acks.All
+            };
+
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+            await producer.ProduceAsync(topic, message);
+
+            producer.Flush();
+        }
+    }
+}
