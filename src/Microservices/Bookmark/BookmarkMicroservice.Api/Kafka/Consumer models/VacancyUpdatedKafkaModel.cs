9300c6cad4a2ccbd39c4203feee5ea9915ad6e24
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-23 18:55:06 +0400

    Add consumer models

---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Kafka/Consumer models/VacancyUpdatedKafkaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BookmarkMicroservice.Api.Kafka.Consumer_models
+{
+    public class VacancyUpdatedKafkaModel
+    {
+        public Guid VacancyId { get; set; }
+        public string NewPosition { get;set; }
+        public int? NewSalaryFrom { get; set; }
+        public int? NewSalaryTo { get; set; }
+        public string? NewWorkExperience { get; set; }
+        public string NewVacancyCity { get; set; }
+    }
+}
