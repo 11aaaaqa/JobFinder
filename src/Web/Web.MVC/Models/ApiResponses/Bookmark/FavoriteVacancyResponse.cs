c15293a0ee148db25a3e356ee19ce9ec7309fdfc
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-30 18:53:11 +0400

    Add response model

---
+++ src/Web/Web.MVC/Models/ApiResponses/Bookmark/FavoriteVacancyResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Web.MVC.Models.ApiResponses.Bookmark
+{
+    public class FavoriteVacancyResponse
+    {
+        public Guid Id { get; set; }
+        public Guid EmployeeId { get; set; }
+        public Guid VacancyId { get; set; }
+        public string Position { get; set; }
+        public int? SalaryFrom { get; set; }
+        public int? SalaryTo { get; set; }
+        public string? WorkExperience { get; set; }
+        public string CompanyName { get; set; }
+        public string VacancyCity { get; set; }
+    }
+}
