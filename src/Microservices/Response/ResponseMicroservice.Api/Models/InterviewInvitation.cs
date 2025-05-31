d212eddd59d66ccbf6850c68d743394cd5e3ebfb
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-31 17:59:59 +0400

    Add models

---
+++ src/Microservices/Response/ResponseMicroservice.Api/Models/InterviewInvitation.cs
@@ -0,0 +1,24 @@
+﻿namespace ResponseMicroservice.Api.Models
+{
+    public class InterviewInvitation
+    {
+        public Guid Id { get; set; }
+        public Guid InvitedCompanyId { get; set; }
+        public Guid EmployeeId { get; set; }
+        public DateTime InvitationDate { get; set; }
+
+        public Guid EmployeeResumeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeSurname { get; set; }
+        public DateOnly? EmployeeDateOfBirth { get; set; }
+        public TimeSpan EmployeeWorkingExperience { get; set; }
+        public string? EmployeeCity { get; set; }
+
+        public Guid VacancyId { get; set; }
+        public string VacancyPosition { get; set; }
+        public int? VacancySalaryFrom { get; set; }
+        public int? VacancySalaryTo { get; set; }
+        public string? VacancyWorkExperience { get; set; }
+        public string VacancyCity { get; set; }
+    }
+}
