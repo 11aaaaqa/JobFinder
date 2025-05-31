b2edc9b4396f14ee703d8d1dd62321259c50ff09
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-31 18:00:19 +0400

    Register services and add database context

--- src/Microservices/Response/ResponseMicroservice.Api/Program.cs
+++ src/Microservices/Response/ResponseMicroservice.Api/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ResponseMicroservice.Api.Database;
+using ResponseMicroservice.Api.Services.Interview_invitation_services;
+using ResponseMicroservice.Api.Services.Vacancy_response_services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    x => x.UseNpgsql(builder.Configuration["Database:ConnectionString"]));
+
+builder.Services.AddTransient<IVacancyResponseService, VacancyResponseService>();
+builder.Services.AddTransient<IInterviewInvitationService, InterviewInvitationService>();
 
 builder.Services.AddControllers();
 
