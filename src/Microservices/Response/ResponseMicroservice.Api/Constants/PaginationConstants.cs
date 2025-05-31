178f1e9333d0a11957cfeeae32138920433ec921
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-31 18:00:29 +0400

    Add constants

---
+++ src/Microservices/Response/ResponseMicroservice.Api/Constants/PaginationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace ResponseMicroservice.Api.Constants
+{
+    public class PaginationConstants
+    {
+        public const int VacancyResponsePageSize = 50;
+        public const int InterviewInvitationPageSize = 50;
+    }
+}
---
+++ src/Microservices/Response/ResponseMicroservice.Api/Constants/VacancyResponseStatusConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ResponseMicroservice.Api.Constants
+{
+    public class VacancyResponseStatusConstants
+    {
+        public const string Accepted = "Приглашение";
+        public const string Rejected = "Отказ";
+        public const string Waiting = "На ожидании";
+    }
+}
