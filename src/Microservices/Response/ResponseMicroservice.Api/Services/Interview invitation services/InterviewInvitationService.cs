1bddbbaec4172a9586408f9012d6bce989dd62ec
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-31 18:00:07 +0400

    Add services

---
+++ src/Microservices/Response/ResponseMicroservice.Api/Services/Interview invitation services/InterviewInvitationService.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResponseMicroservice.Api.Constants;
+using ResponseMicroservice.Api.Database;
+using ResponseMicroservice.Api.Enums;
+using ResponseMicroservice.Api.Models;
+
+namespace ResponseMicroservice.Api.Services.Interview_invitation_services
+{
+    public class InterviewInvitationService(ApplicationDbContext context) : IInterviewInvitationService
+    {
+        public async Task<List<InterviewInvitation>> GetInterviewInvitationsByCompanyIdAsync(Guid companyId, DateTimeOrderByType orderByTimeType, int pageNumber)
+        {
+            var interviewInvitations =
+                context.InterviewInvitations.Where(x => x.InvitedCompanyId == companyId).AsQueryable();
+
+            switch (orderByTimeType)
+            {
+                case DateTimeOrderByType.Ascending:
+                    interviewInvitations = interviewInvitations.OrderBy(x => x.InvitationDate);
+                    break;
+                case DateTimeOrderByType.Descending:
+                    interviewInvitations = interviewInvitations.OrderByDescending(x => x.InvitationDate);
+                    break;
+            }
+
+            return await interviewInvitations.Skip((pageNumber - 1) * PaginationConstants.InterviewInvitationPageSize)
+                .Take(PaginationConstants.InterviewInvitationPageSize).ToListAsync();
+        }
+
+        public async Task<List<InterviewInvitation>> GetInterviewInvitationsByEmployeeIdAsync(Guid employeeId, string? searchingQuery,
+            DateTimeOrderByType orderByTimeType, int pageNumber)
+        {
+            var interviewInvitations =
+                context.InterviewInvitations.Where(x => x.EmployeeId == employeeId).AsQueryable();
+
+            if (searchingQuery is not null)
+                interviewInvitations = interviewInvitations.Where(x => x.VacancyPosition.ToLower().Contains(searchingQuery.ToLower()));
+
+            switch (orderByTimeType)
+            {
+                case DateTimeOrderByType.Ascending:
+                    interviewInvitations = interviewInvitations.OrderBy(x => x.InvitationDate);
+                    break;
+                case DateTimeOrderByType.Descending:
+                    interviewInvitations = interviewInvitations.OrderByDescending(x => x.InvitationDate);
+                    break;
+            }
+
+            return await interviewInvitations.Skip((pageNumber - 1) * PaginationConstants.InterviewInvitationPageSize)
+                .Take(PaginationConstants.InterviewInvitationPageSize).ToListAsync();
+        }
+
+        public async Task<List<InterviewInvitation>> GetCompanyInterviewInvitationsByVacancyIdAsync(Guid vacancyId, DateTimeOrderByType orderByTimeType,
+            int pageNumber)
+        {
+            var interviewInvitations =
+                context.InterviewInvitations.Where(x => x.VacancyId == vacancyId).AsQueryable();
+
+            switch (orderByTimeType)
+            {
+                case DateTimeOrderByType.Ascending:
+                    interviewInvitations = interviewInvitations.OrderBy(x => x.InvitationDate);
+                    break;
+                case DateTimeOrderByType.Descending:
+                    interviewInvitations = interviewInvitations.OrderByDescending(x => x.InvitationDate);
+                    break;
+            }
+
+            return await interviewInvitations.Skip((pageNumber - 1) * PaginationConstants.InterviewInvitationPageSize)
+                .Take(PaginationConstants.InterviewInvitationPageSize).ToListAsync();
+        }
+
+        public async Task AddInvitationAsync(InterviewInvitation model)
+        {
+            await context.InterviewInvitations.AddAsync(model);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Microservices/Response/ResponseMicroservice.Api/Services/Vacancy response services/IVacancyResponseService.cs
@@ -0,0 +1,16 @@
+﻿using ResponseMicroservice.Api.Enums;
+using ResponseMicroservice.Api.Models;
+
+namespace ResponseMicroservice.Api.Services.Vacancy_response_services
+{
+    public interface IVacancyResponseService
+    {
+        Task<List<VacancyResponse>> GetVacancyResponsesByEmployeeIdAsync(Guid employeeId, string? searchingQuery,
+            DateTimeOrderByType orderByTimeType, int pageNumber);
+        Task<List<VacancyResponse>> GetVacancyResponsesByCompanyIdAsync(Guid companyId, DateTimeOrderByType orderByTimeType, int pageNumber);
+        Task<List<VacancyResponse>> GetCompanyVacancyResponsesByVacancyIdAsync(Guid vacancyId, DateTimeOrderByType orderByTimeType, int pageNumber);
+        Task SetVacancyResponseStatusAsync(Guid vacancyResponseId, string status);
+        Task AddVacancyResponseAsync(VacancyResponse model);
+        Task<bool> HasEmployeeRespondedToVacancyAsync(Guid employeeId, Guid vacancyId);
+    }
+}
