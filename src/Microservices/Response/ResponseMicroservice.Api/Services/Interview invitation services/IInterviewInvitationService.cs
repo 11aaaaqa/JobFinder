74e685846d71c5456c7ea3a2f773a3e1a220d62d
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-05-31 18:00:07 +0400

    Add services

---
+++ src/Microservices/Response/ResponseMicroservice.Api/Services/Interview invitation services/IInterviewInvitationService.cs
@@ -0,0 +1,14 @@
+﻿using ResponseMicroservice.Api.Enums;
+using ResponseMicroservice.Api.Models;
+
+namespace ResponseMicroservice.Api.Services.Interview_invitation_services
+{
+    public interface IInterviewInvitationService
+    {
+        Task<List<InterviewInvitation>> GetInterviewInvitationsByCompanyIdAsync(Guid companyId, DateTimeOrderByType orderByTimeType, int pageNumber);
+        Task<List<InterviewInvitation>> GetInterviewInvitationsByEmployeeIdAsync(Guid employeeId, string? searchingQuery,
+            DateTimeOrderByType orderByTimeType, int pageNumber);
+        Task<List<InterviewInvitation>> GetCompanyInterviewInvitationsByVacancyIdAsync(Guid vacancyId, DateTimeOrderByType orderByTimeType, int pageNumber);
+        Task AddInvitationAsync(InterviewInvitation model);
+    }
+}
