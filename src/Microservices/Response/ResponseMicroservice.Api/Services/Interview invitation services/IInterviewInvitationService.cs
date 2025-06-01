cb936fec61c2cd30ee67f257d789447a7e997224
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-01 14:59:25 +0400

    Add getting single object by id method

--- src/Microservices/Response/ResponseMicroservice.Api/Services/Interview invitation services/IInterviewInvitationService.cs
+++ src/Microservices/Response/ResponseMicroservice.Api/Services/Interview invitation services/IInterviewInvitationService.cs
@@ -5,6 +5,7 @@ namespace ResponseMicroservice.Api.Services.Interview_invitation_services
 {
     public interface IInterviewInvitationService
     {
+        Task<InterviewInvitation?> GetInterviewInvitationByIdAsync(Guid interviewInvitationId);
         Task<List<InterviewInvitation>> GetInterviewInvitationsByCompanyIdAsync(Guid companyId, DateTimeOrderByType orderByTimeType, int pageNumber);
         Task<List<InterviewInvitation>> GetInterviewInvitationsByEmployeeIdAsync(Guid employeeId, string? searchingQuery,
             DateTimeOrderByType orderByTimeType, int pageNumber);
--- src/Microservices/Response/ResponseMicroservice.Api/Services/Vacancy response services/IVacancyResponseService.cs
+++ src/Microservices/Response/ResponseMicroservice.Api/Services/Vacancy response services/IVacancyResponseService.cs
@@ -5,6 +5,7 @@ namespace ResponseMicroservice.Api.Services.Vacancy_response_services
 {
     public interface IVacancyResponseService
     {
+        Task<VacancyResponse?> GetVacancyResponseByIdAsync(Guid vacancyResponseId);
         Task<List<VacancyResponse>> GetVacancyResponsesByEmployeeIdAsync(Guid employeeId, string? searchingQuery,
             DateTimeOrderByType orderByTimeType, int pageNumber);
         Task<List<VacancyResponse>> GetVacancyResponsesByCompanyIdAsync(Guid companyId, DateTimeOrderByType orderByTimeType, int pageNumber);
