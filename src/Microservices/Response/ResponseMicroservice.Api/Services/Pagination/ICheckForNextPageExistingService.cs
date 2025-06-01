ec877247dde7fbd1c93c211c362c1dddc7a3b4b7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-01 14:26:22 +0400

    Add pagination checking service

---
+++ src/Microservices/Response/ResponseMicroservice.Api/Services/Pagination/ICheckForNextPageExistingService.cs
@@ -0,0 +1,16 @@
+﻿using ResponseMicroservice.Api.Enums;
+
+namespace ResponseMicroservice.Api.Services.Pagination
+{
+    public interface ICheckForNextPageExistingService
+    {
+        Task<bool> DoesNextVacancyResponsesByEmployeeIdPageExistAsync(Guid employeeId, string? searchingQuery,
+            DateTimeOrderByType orderByTimeType, int currentPageNumber);
+        Task<bool> DoesNextVacancyResponsesByCompanyIdPageExistAsync(Guid companyId, DateTimeOrderByType orderByTimeType, int currentPageNumber);
+        Task<bool> DoesNextCompanyVacancyResponsesByVacancyIdPageExistAsync(Guid vacancyId, DateTimeOrderByType orderByTimeType, int currentPageNumber);
+        Task<bool> DoesNextInterviewInvitationsByCompanyIdPageExistAsync(Guid companyId, DateTimeOrderByType orderByTimeType, int currentPageNumber);
+        Task<bool> DoesNextInterviewInvitationsByEmployeeIdPageExistAsync(Guid employeeId, string? searchingQuery,
+            DateTimeOrderByType orderByTimeType, int currentPageNumber);
+        Task<bool> DoesNextCompanyInterviewInvitationsByVacancyIdPageExistAsync(Guid vacancyId, DateTimeOrderByType orderByTimeType, int currentPageNumber);
+    }
+}
