99e86c9c8df95e00edd2be7bf2ca7c4b5fd64ec5
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-04 17:07:47 +0400

    Add response models

---
+++ src/Web/Web.MVC/Models/ApiResponses/Employee/VacancyResponseResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace Web.MVC.Models.ApiResponses.Employee
+{
+    public class VacancyResponseResponse
+    {
+        public Guid Id { get; set; }
+        public Guid VacancyCompanyId { get; set; }
+        public Guid EmployeeId { get; set; }
+        public string ResponseStatus { get; set; }
+        public DateTime ResponseDate { get; set; }
+
+        public Guid RespondedEmployeeResumeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeSurname { get; set; }
+        public DateOnly? EmployeeDateOfBirth { get; set; }
+        public TimeSpan EmployeeWorkingExperience { get; set; }
+        public string? EmployeeCity { get; set; }
+
+        public Guid VacancyId { get; set; }
+        public string VacancyPosition { get; set; }
+        public int? VacancySalaryFrom { get; set; }
+        public int? VacancySalaryTo { get; set; }
+        public string? VacancyWorkExperience { get; set; }
+        public string VacancyCity { get; set; }
+    }
+}
