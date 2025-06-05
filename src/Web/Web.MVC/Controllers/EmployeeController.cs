78792fea53349a207fa343d1234cd75a460e6866
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-05 15:32:12 +0400

    Add getting employee responses

--- src/Web/Web.MVC/Controllers/EmployeeController.cs
+++ src/Web/Web.MVC/Controllers/EmployeeController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Text;
 using System.Text.Json;
 using System.Web;
+using GeneralLibrary.Enums;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Web.MVC.DTOs.Employee;
 using Web.MVC.Models.ApiResponses;
 using Web.MVC.Models.ApiResponses.Bookmark;
+using Web.MVC.Models.ApiResponses.Employee;
 using Web.MVC.Models.ApiResponses.Vacancy;
 
 namespace Web.MVC.Controllers
@@ -102,6 +104,36 @@ namespace Web.MVC.Controllers
             ViewBag.EmployeeId = employeeId;
 
             return View(vacancies);
+        }
+
+        [Authorize]
+        [Route("employee/responses")]
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeeVacancyResponses(string? query, DateTimeOrderByType timeSort, int index = 1)
+        {
+            using HttpClient httpClient = httpClientFactory.CreateClient();
+            var encodedQuery = HttpUtility.UrlEncode(query);
+
+            var employeeResponse = await httpClient.GetAsync($"{url}/api/Employee/GetEmployeeByEmail?email={User.Identity.Name}");
+            employeeResponse.EnsureSuccessStatusCode();
+            var employee = await employeeResponse.Content.ReadFromJsonAsync<EmployeeResponse>();
+
+            var vacancyResponsesResponse = await httpClient.GetAsync(
+                $"{url}/api/VacancyResponse/GetVacancyResponsesByEmployeeId/{employee.Id}?searchingQuery={encodedQuery}&orderByTimeType={timeSort}&pageNumber={index}");
+            vacancyResponsesResponse.EnsureSuccessStatusCode();
+            var vacancyResponses = await vacancyResponsesResponse.Content.ReadFromJsonAsync<List<VacancyResponseResponse>>();
+
+            var doesNextPageExistResponse = await httpClient.GetAsync(
+                $"{url}/api/VacancyResponse/DoesNextVacancyResponsesByEmployeeIdPageExist/{employee.Id}?searchingQuery={encodedQuery}&orderByTimeType={timeSort}&currentPageNumber={index}");
+            doesNextPageExistResponse.EnsureSuccessStatusCode();
+            bool doesNextPageExist = await doesNextPageExistResponse.Content.ReadFromJsonAsync<bool>();
+
+            ViewBag.DoesNextPageExist = doesNextPageExist;
+            ViewBag.CurrentPageNumber = index;
+            ViewBag.SearchingQuery = query;
+            ViewBag.TimeSort = timeSort;
+
+            return View(vacancyResponses);
         }
     }
 }
