1d53e41b54fc20e2a28bdfc3464bd49ba1871afe
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-09 18:51:10 +0400

    Ad dto

---
+++ src/Microservices/Response/ResponseMicroservice.Api/DTOs/AddInterviewInvitationDto.cs
@@ -0,0 +1,24 @@
+﻿namespace ResponseMicroservice.Api.DTOs
+{
+    public class AddInterviewInvitationDto
+    {
+        public Guid InvitedCompanyId { get; set; }
+        public Guid EmployeeId { get; set; }
+
+        public Guid EmployeeResumeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeSurname { get; set; }
+        public uint? EmployeeDesiredSalary { get; set; }
+        public DateOnly? EmployeeDateOfBirth { get; set; }
+        public TimeSpan EmployeeWorkingExperience { get; set; }
+        public string? EmployeeCity { get; set; }
+
+        public Guid VacancyId { get; set; }
+        public string VacancyPosition { get; set; }
+        public int? VacancySalaryFrom { get; set; }
+        public int? VacancySalaryTo { get; set; }
+        public string? VacancyWorkExperience { get; set; }
+        public string VacancyCity { get; set; }
+        public string VacancyCompanyName { get; set; }
+    }
+}
