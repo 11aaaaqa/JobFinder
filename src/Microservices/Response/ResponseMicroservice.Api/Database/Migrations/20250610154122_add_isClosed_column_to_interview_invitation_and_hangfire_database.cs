b6fa86f0b5e029c62d26a3ecf646c50c4f9edc85
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-10 20:50:03 +0400

    Add migration

---
+++ src/Microservices/Response/ResponseMicroservice.Api/Database/Migrations/20250610154122_add_isClosed_column_to_interview_invitation_and_hangfire_database.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResponseMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_isClosed_column_to_interview_invitation_and_hangfire_database : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsClosed",
+                table: "InterviewInvitations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsClosed",
+                table: "InterviewInvitations");
+        }
+    }
+}
