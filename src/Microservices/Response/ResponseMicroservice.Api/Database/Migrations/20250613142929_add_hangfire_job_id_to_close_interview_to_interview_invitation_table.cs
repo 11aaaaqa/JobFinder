6c1f4afbfad1eba6a34d2f3f560860bbe52b1b1b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-13 18:54:57 +0400

    Add migration

---
+++ src/Microservices/Response/ResponseMicroservice.Api/Database/Migrations/20250613142929_add_hangfire_job_id_to_close_interview_to_interview_invitation_table.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResponseMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_hangfire_job_id_to_close_interview_to_interview_invitation_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HangfireDelayedJobId",
+                table: "InterviewInvitations",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HangfireDelayedJobId",
+                table: "InterviewInvitations");
+        }
+    }
+}
