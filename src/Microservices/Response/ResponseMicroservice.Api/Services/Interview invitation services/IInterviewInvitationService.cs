eb54736d646a7b1ca1ca13fa71c1ccf98a091fcc
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-14 19:41:11 +0400

    Add searching to company interview invitations

--- src/Microservices/Response/ResponseMicroservice.Api/Services/Interview invitation services/IInterviewInvitationService.cs
+++ src/Microservices/Response/ResponseMicroservice.Api/Services/Interview invitation services/IInterviewInvitationService.cs
@@ -6,10 +6,12 @@ namespace ResponseMicroservice.Api.Services.Interview_invitation_services
     public interface IInterviewInvitationService
     {
         Task<InterviewInvitation?> GetInterviewInvitationByIdAsync(Guid interviewInvitationId);
-        Task<List<InterviewInvitation>> GetInterviewInvitationsByCompanyIdAsync(Guid companyId, DateTimeOrderByType orderByTimeType, int pageNumber);
+        Task<List<InterviewInvitation>> GetInterviewInvitationsByCompanyIdAsync(Guid companyId, string? searchingQuery, DateTimeOrderByType orderByTimeType,
+            int pageNumber);
         Task<List<InterviewInvitation>> GetInterviewInvitationsByEmployeeIdAsync(Guid employeeId, string? searchingQuery,
             DateTimeOrderByType orderByTimeType, int pageNumber);
-        Task<List<InterviewInvitation>> GetCompanyInterviewInvitationsByVacancyIdAsync(Guid vacancyId, DateTimeOrderByType orderByTimeType, int pageNumber);
+        Task<List<InterviewInvitation>> GetCompanyInterviewInvitationsByVacancyIdAsync(Guid vacancyId, string? searchingQuery, DateTimeOrderByType orderByTimeType,
+            int pageNumber);
         Task AddInvitationAsync(InterviewInvitation model);
         Task<InterviewInvitation?> GetInterviewInvitationAsync(Guid employeeId, Guid companyId);
         Task CloseInterviewAsync(Guid interviewInvitationId);
