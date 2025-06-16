6bee110f764ae1571d550e7a1e6eb0c5a7b84e44
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-16 12:07:03 +0400

    Add dto

---
+++ src/Microservices/Chat/ChatMicroservice.Api/DTOs/CreateChatDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatMicroservice.Api.DTOs
+{
+    public class CreateChatDto
+    {
+        public Guid EmployeeId { get; set; }
+        public string EmployeeFullName { get; set; }
+
+        public Guid EmployerId { get; set; }
+        public string EmployerFullName { get; set; }
+    }
+}
