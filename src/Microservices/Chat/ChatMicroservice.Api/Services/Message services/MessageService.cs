070c4468994215c2e29becd2c6b51876948e6e62
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-16 11:53:54 +0400

    Add message serivce

---
+++ src/Microservices/Chat/ChatMicroservice.Api/Services/Message services/MessageService.cs
@@ -0,0 +1,27 @@
+﻿using ChatMicroservice.Api.Constants;
+using ChatMicroservice.Api.Database;
+using ChatMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatMicroservice.Api.Services.Message_services
+{
+    public class MessageService(ApplicationDbContext context) : IMessageService
+    {
+        public async Task<List<Message>> GetLastMessagesByChatIdAsync(Guid chatId, int pageNumber)
+        {
+            var messages = await context.Messages
+                .Where(x => x.ChatId == chatId)
+                .OrderByDescending(x => x.CreatedAt)
+                .Skip((pageNumber - 1) * PaginationConstants.MessagesPageSize)
+                .Take(PaginationConstants.MessagesPageSize)
+                .ToListAsync();
+            return messages;
+        }
+
+        public async Task CreateMessageAsync(Message message)
+        {
+            await context.Messages.AddAsync(message);
+            await context.SaveChangesAsync();
+        }
+    }
+}
