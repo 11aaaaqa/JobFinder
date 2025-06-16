f123bc960eddf5b93545ae3f194c20b0f9ebb236
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-16 11:54:00 +0400

    Add method

--- src/Microservices/Chat/ChatMicroservice.Api/Services/Chat services/ChatService.cs
+++ src/Microservices/Chat/ChatMicroservice.Api/Services/Chat services/ChatService.cs
@@ -11,6 +11,7 @@ namespace ChatMicroservice.Api.Services.Chat_services
         {
             var chats = await context.Chats
                 .Where(x => x.EmployeeId == employeeId)
+                .OrderBy(x => x.LastMessageSendingTime)
                 .Skip((pageNumber - 1) * PaginationConstants.ChatsPageSize)
                 .Take(PaginationConstants.ChatsPageSize)
                 .ToListAsync();
@@ -21,6 +22,7 @@ namespace ChatMicroservice.Api.Services.Chat_services
         {
             var chats = await context.Chats
                 .Where(x => x.EmployerId == employerId)
+                .OrderBy(x => x.LastMessageSendingTime)
                 .Skip((pageNumber - 1) * PaginationConstants.ChatsPageSize)
                 .Take(PaginationConstants.ChatsPageSize)
                 .ToListAsync();
@@ -30,6 +32,16 @@ namespace ChatMicroservice.Api.Services.Chat_services
         public async Task CreateChatAsync(Chat chat)
         {
             await context.Chats.AddAsync(chat);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateLastMessageSendingTimeAsync(Guid chatId)
+        {
+            var chat = await context.Chats.SingleOrDefaultAsync(x => x.Id == chatId);
+            if(chat is null)
+                return;
+
+            chat.LastMessageSendingTime = DateTime.UtcNow;
             await context.SaveChangesAsync();
         }
     }
