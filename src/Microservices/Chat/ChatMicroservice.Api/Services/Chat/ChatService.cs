1016b21a6f228e33ec3d67d4eae8acd6150300b4
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-16 11:32:09 +0400

    Add chat service

---
+++ src/Microservices/Chat/ChatMicroservice.Api/Services/Chat/ChatService.cs
@@ -0,0 +1,36 @@
+﻿using ChatMicroservice.Api.Constants;
+using ChatMicroservice.Api.Database;
+using ChatMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatMicroservice.Api.Services.Chat
+{
+    public class ChatService(ApplicationDbContext context) : IChatService
+    {
+        public async Task<List<ChatModel>> GetChatListByEmployeeIdAsync(Guid employeeId, int pageNumber)
+        {
+            var chats = await context.Chats
+                .Where(x => x.EmployeeId == employeeId)
+                .Skip((pageNumber - 1) * PaginationConstants.ChatsPageSize)
+                .Take(PaginationConstants.ChatsPageSize)
+                .ToListAsync();
+            return chats;
+        }
+
+        public async Task<List<ChatModel>> GetChatListByEmployerIdAsync(Guid employerId, int pageNumber)
+        {
+            var chats = await context.Chats
+                .Where(x => x.EmployerId == employerId)
+                .Skip((pageNumber - 1) * PaginationConstants.ChatsPageSize)
+                .Take(PaginationConstants.ChatsPageSize)
+                .ToListAsync();
+            return chats;
+        }
+
+        public async Task CreateChatAsync(ChatModel chat)
+        {
+            await context.Chats.AddAsync(chat);
+            await context.SaveChangesAsync();
+        }
+    }
+}
