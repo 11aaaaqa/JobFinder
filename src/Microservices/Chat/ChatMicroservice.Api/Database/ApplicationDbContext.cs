40dff46a7c157fa2a3d0b297e0de690ac6323f3f
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-16 11:31:48 +0400

    Add database context

---
+++ src/Microservices/Chat/ChatMicroservice.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using ChatMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatMicroservice.Api.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<ChatModel> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ChatModel>()
+                .HasMany(x => x.Messages)
+                .WithOne(x => x.Chat)
+                .HasForeignKey(x => x.ChatId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
