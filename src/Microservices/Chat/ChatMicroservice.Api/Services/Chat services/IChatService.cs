8b890e22aae8ce78f20136ef614e62915d2ec02f
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-16 14:31:19 +0400

    Add a few more methods and update existing

--- src/Microservices/Chat/ChatMicroservice.Api/Services/Chat services/IChatService.cs
+++ src/Microservices/Chat/ChatMicroservice.Api/Services/Chat services/IChatService.cs
@@ -4,6 +4,7 @@ namespace ChatMicroservice.Api.Services.Chat_services
 {
     public interface IChatService
     {
+        Task<Chat?> GetChatByIdAsync(Guid chatId);
         Task<List<Chat>> GetChatListByEmployeeIdAsync(Guid employeeId, int pageNumber);
         Task<List<Chat>> GetChatListByEmployerIdAsync(Guid employerId, int pageNumber);
         Task CreateChatAsync(Chat chat);
