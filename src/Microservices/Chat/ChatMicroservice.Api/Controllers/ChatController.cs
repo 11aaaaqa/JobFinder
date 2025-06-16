6927fc85ac9426a6c06a536b75625802d0db4ec6
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-16 12:07:10 +0400

    Add chat controller

---
+++ src/Microservices/Chat/ChatMicroservice.Api/Controllers/ChatController.cs
@@ -0,0 +1,36 @@
+﻿using ChatMicroservice.Api.DTOs;
+using ChatMicroservice.Api.Models;
+using ChatMicroservice.Api.Services.Chat_services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatMicroservice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController(IChatService chatService) : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetChatListByEmployeeId/{employeeId}")]
+        public async Task<IActionResult> GetChatListByEmployeeIdAsync(Guid employeeId, int pageNumber)
+            => Ok(await chatService.GetChatListByEmployeeIdAsync(employeeId, pageNumber));
+
+        [HttpGet]
+        [Route("GetChatListByEmployerId/{employerId}")]
+        public async Task<IActionResult> GetChatListByEmployerIdAsync(Guid employerId, int pageNumber)
+            => Ok(await chatService.GetChatListByEmployerIdAsync(employerId, pageNumber));
+
+        [HttpPost]
+        [Route("CreateChat")]
+        public async Task<IActionResult> CreateChatAsync([FromBody] CreateChatDto model)
+        {
+            await chatService.CreateChatAsync(new Chat
+            {
+                EmployeeFullName = model.EmployeeFullName, EmployeeId = model.EmployeeId,
+                EmployerFullName = model.EmployerFullName, EmployerId = model.EmployerId,
+                Id = Guid.NewGuid(), LastMessageSendingTime = DateTime.UtcNow
+            });
+
+            return Ok();
+        }
+    }
+}
