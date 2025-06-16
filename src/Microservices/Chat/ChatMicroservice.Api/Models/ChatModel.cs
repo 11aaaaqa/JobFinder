99f66549a6406a8c5ea43403a8f8a40129ccc86a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-16 11:31:42 +0400

    Add models

---
+++ src/Microservices/Chat/ChatMicroservice.Api/Models/ChatModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ChatMicroservice.Api.Models
+{
+    public class ChatModel
+    {
+        public Guid Id { get; set; }
+
+        public Guid EmployeeId { get; set; }
+        public string EmployeeFullName { get; set; }
+
+        public Guid EmployerId { get; set; }
+        public string EmployerFullName { get; set; }
+
+        public List<Message> Messages { get; set; }
+    }
+}
