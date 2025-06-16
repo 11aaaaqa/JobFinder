50c834e41366b251f73c83700e13aa4fa92e5c9b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-16 11:31:36 +0400

    Add constants

---
+++ src/Microservices/Chat/ChatMicroservice.Api/Constants/PaginationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatMicroservice.Api.Constants
+{
+    public class PaginationConstants
+    {
+        public const int ChatsPageSize = 50;
+        public const int MessagesPageSize = 70;
+    }
+}
