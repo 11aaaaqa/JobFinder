4d3281ab71365e37a8b9d689963dc5702b5cd3dc
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-17 12:59:23 +0400

    Update dto

--- src/Microservices/Chat/ChatMicroservice.Api/DTOs/CreateMessageDto.cs
+++ src/Microservices/Chat/ChatMicroservice.Api/DTOs/CreateMessageDto.cs
@@ -5,6 +5,5 @@
         public Guid ChatId { get; set; }
         public Guid SenderId { get; set; }
         public string Text { get; set; }
-        public DateTime CreatedAt { get; set; }
     }
 }
