8706fdce36c31209ad980fab6c71bb719d91e83a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-19 14:38:59 +0400

    Add view model

---
+++ src/Web/Web.MVC/Models/View models/GetChatByIdViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Web.MVC.Models.View_models
+{
+    public class GetChatByIdViewModel
+    {
+        public Guid CurrentId { get; set; }
+        public string CurrentEmail { get; set; }
+        public Guid ChatId { get; set; }
+        public string ReceiverEmail { get; set; }
+        public string InterlocutorFullName { get; set; }
+        public string? InterlocutorCompanyName { get; set; }
+    }
+}
