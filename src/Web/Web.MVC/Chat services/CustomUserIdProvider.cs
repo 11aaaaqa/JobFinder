dea985c5881e673dcde6b56288fac5e70ff1b8a4
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-19 14:38:10 +0400

    Add userId provider

---
+++ src/Web/Web.MVC/Chat services/CustomUserIdProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Web.MVC.Chat_services
+{
+    public class CustomUserIdProvider : IUserIdProvider
+    {
+        public string? GetUserId(HubConnectionContext connection)
+        {
+            return connection.User.FindFirst(ClaimTypes.Email)?.Value;
+        }
+    }
+}
