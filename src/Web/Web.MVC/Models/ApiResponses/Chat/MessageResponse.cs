e73c9fcff20877381c024659f5bf19d08f2e9fcb
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-19 14:39:06 +0400

    Add models

---
+++ src/Web/Web.MVC/Models/ApiResponses/Chat/MessageResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Web.MVC.Models.ApiResponses.Chat
+{
+    public class MessageResponse
+    {
+        public Guid Id { get; set; }
+        public Guid ChatId { get; set; }
+        public ChatResponse Chat { get; set; }
+        public Guid SenderId { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
