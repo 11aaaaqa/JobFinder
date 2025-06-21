6a7c6a5a3defa907844ee6dcfbc9df393616ecd8
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-21 15:44:28 +0400

    Add kafka consumer

---
+++ src/Microservices/Resume/ResumeMicroservice.Api/Kafka/Consumers/EmployeeUpdatedKafkaConsumer.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.Json;
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using Microsoft.EntityFrameworkCore;
+using ResumeMicroservice.Api.Constants;
+using ResumeMicroservice.Api.Database;
+using ResumeMicroservice.Api.Kafka.Consumer_models;
+
+namespace ResumeMicroservice.Api.Kafka.Consumers
+{
+    public class EmployeeUpdatedKafkaConsumer(IConfiguration configuration, IServiceScopeFactory scopeFactory) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            const string topicName = "employee-updated-topic";
+            var config = new ConsumerConfig
+            {
+                GroupId = KafkaConstants.GroupId,
+                BootstrapServers = configuration["Kafka:BootstrapServers"],
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            using var adminClient = new AdminClientBuilder(config).Build();
+            var metadata = adminClient.GetMetadata(TimeSpan.FromSeconds(10));
+            bool topicExists = metadata.Topics.Exists(x => x.Topic == topicName);
+            if (!topicExists)
+            {
+                await adminClient.CreateTopicsAsync(new List<TopicSpecification>
+                {
+                    new() { Name = topicName, NumPartitions = 1, ReplicationFactor = 1 }
+                });
+            }
+
+            using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+            consumer.Subscribe(topicName);
+            
+            using var scope = scopeFactory.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var consumeResult = consumer.Consume(stoppingToken);
+                var model = JsonSerializer.Deserialize<EmployeeUpdatedConsumerModel>(consumeResult.Message.Value);
+
+                var resumesToUpdate = await context.Resumes
+                    .Where(x => x.EmployeeId == model.EmployeeId)
+                    .ToListAsync(CancellationToken.None);
+
+                foreach (var resume in resumesToUpdate)
+                {
+                    resume.Name = model.NewName;
+                    resume.Surname = model.NewSurname;
+                    resume.Patronymic = model.NewPatronymic;
+                    resume.Gender = model.NewGender;
+                    resume.DateOfBirth = model.NewDateOfBirth;
+                }
+                await context.SaveChangesAsync(CancellationToken.None);
+            }
+            consumer.Close();
+        }
+    }
+}
