371378d9dbe15343c8992cb52396b4c6bfa521ea
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-21 15:44:20 +0400

    Add consumer models

---
+++ src/Microservices/Resume/ResumeMicroservice.Api/Kafka/Consumer models/EmployeeUpdatedConsumerModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ResumeMicroservice.Api.Kafka.Consumer_models
+{
+    public class EmployeeUpdatedConsumerModel
+    {
+        public Guid EmployeeId { get; set; }
+        public string NewName { get; set; }
+        public string NewSurname { get; set; }
+        public string? NewPatronymic { get; set; }
+        public string? NewGender { get; set; }
+        public DateOnly? NewDateOfBirth { get; set; }
+    }
+}
