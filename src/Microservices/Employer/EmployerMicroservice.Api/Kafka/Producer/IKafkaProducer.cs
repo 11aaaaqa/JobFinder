e99d9f6452a372fb2f173b47a85a1f0bd6d79c8a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-21 14:28:12 +0400

    Add kafka producer

---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Kafka/Producer/IKafkaProducer.cs
@@ -0,0 +1,9 @@
+﻿using Confluent.Kafka;
+
+namespace EmployerMicroservice.Api.Kafka.Producer
+{
+    public interface IKafkaProducer
+    {
+        Task ProduceAsync(string topicName, Message<Null, string> message);
+    }
+}
---
+++ src/Microservices/Employer/EmployerMicroservice.Api/Kafka/Producer/KafkaProducer.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+
+namespace EmployerMicroservice.Api.Kafka.Producer
+{
+    public class KafkaProducer(IConfiguration configuration) : IKafkaProducer
+    {
+        public async Task ProduceAsync(string topicName, Message<Null, string> message)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = configuration["Kafka:BootstrapServers"],
+                Acks = Acks.All
+            };
+            using var adminClient = new AdminClientBuilder(config).Build();
+            var metadata = adminClient.GetMetadata(TimeSpan.FromSeconds(10));
+            bool topicExists = metadata.Topics.Exists(x => x.Topic == topicName);
+            if (!topicExists)
+            {
+                await adminClient.CreateTopicsAsync(new List<TopicSpecification>
+                {
+                    new (){Name = topicName, NumPartitions = 1, ReplicationFactor = 1}
+                });
+            }
+
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+            await producer.ProduceAsync(topicName, message);
+            producer.Flush(TimeSpan.FromSeconds(10));
+        }
+    }
+}
