a283a6b6585673c71d5a93acbdfce9f881d2e7cf
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-29 20:08:51 +0400

    Add unread messages count property

--- src/Microservices/Chat/ChatMicroservice.Api/Models/Chat.cs
+++ src/Microservices/Chat/ChatMicroservice.Api/Models/Chat.cs
@@ -10,6 +10,7 @@
         public Guid EmployerId { get; set; }
         public string EmployerFullName { get; set; }
         public DateTime LastMessageSendingTime { get; set; }
+        public int UnreadMessagesCount { get; set; }
 
         public List<Message> Messages { get; set; } = new();
     }
