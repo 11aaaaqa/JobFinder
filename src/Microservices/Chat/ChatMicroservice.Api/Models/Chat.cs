aecf01ace0dfa9ec6b7c5630905fa5a2544b11ac
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-06-30 20:22:52 +0400

    Add separated unread messages count for both interlocutors

--- src/Microservices/Chat/ChatMicroservice.Api/Models/Chat.cs
+++ src/Microservices/Chat/ChatMicroservice.Api/Models/Chat.cs
@@ -6,11 +6,12 @@
 
         public Guid EmployeeId { get; set; }
         public string EmployeeFullName { get; set; }
+        public int EmployeeUnreadMessagesCount { get; set; } = 0;
 
         public Guid EmployerId { get; set; }
         public string EmployerFullName { get; set; }
+        public int EmployerUnreadMessagesCount { get; set; } = 0;
         public DateTime LastMessageSendingTime { get; set; }
-        public int UnreadMessagesCount { get; set; }
 
         public List<Message> Messages { get; set; } = new();
     }
