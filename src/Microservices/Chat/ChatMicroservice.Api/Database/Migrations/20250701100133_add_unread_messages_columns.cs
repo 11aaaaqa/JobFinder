9d1f8e8514334a13e921107b8af3d397ca53ceeb
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-01 20:59:26 +0400

    Add migration

---
+++ src/Microservices/Chat/ChatMicroservice.Api/Database/Migrations/20250701100133_add_unread_messages_columns.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_unread_messages_columns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRead",
+                table: "Messages",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeUnreadMessagesCount",
+                table: "Chats",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmployerUnreadMessagesCount",
+                table: "Chats",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRead",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeUnreadMessagesCount",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "EmployerUnreadMessagesCount",
+                table: "Chats");
+        }
+    }
+}
