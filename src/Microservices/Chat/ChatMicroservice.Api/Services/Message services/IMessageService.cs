75b71c7bc959b59e1025c6f92332d334cfcd9571
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-02 20:25:41 +0400

    Add getting unread messages and marking messages as read methods

--- src/Microservices/Chat/ChatMicroservice.Api/Services/Message services/IMessageService.cs
+++ src/Microservices/Chat/ChatMicroservice.Api/Services/Message services/IMessageService.cs
@@ -7,5 +7,9 @@ namespace ChatMicroservice.Api.Services.Message_services
         Task<Message?> GetMessageByIdAsync(Guid messageId);
         Task<List<Message>> GetLastMessagesByChatIdAsync(Guid chatId, int pageNumber);
         Task CreateMessageAsync(Message message);
+        Task<List<Message>> GetFirstUnreadMessagesAsync(Guid chatId, Guid? currentEmployerId, Guid? currentEmployeeId);
+        Task<List<Message>> GetLastReadMessagesAsync(Guid chatId, Guid? currentEmployerId, Guid? currentEmployeeId, int pageNumber);
+        Task<int> MarkMessagesAsReadAsync(List<Message> messages);
+        Task<bool> MarkMessageAsReadAsync(Guid messageId);
     }
 }
--- src/Microservices/Chat/ChatMicroservice.Api/Services/Message services/MessageService.cs
+++ src/Microservices/Chat/ChatMicroservice.Api/Services/Message services/MessageService.cs
@@ -26,5 +26,63 @@ namespace ChatMicroservice.Api.Services.Message_services
             await context.Messages.AddAsync(message);
             await context.SaveChangesAsync();
         }
+
+        public async Task<List<Message>> GetFirstUnreadMessagesAsync(Guid chatId, Guid? currentEmployerId, Guid? currentEmployeeId)
+        {
+            var currentId = currentEmployeeId ?? currentEmployerId;
+            if (currentId == null)
+                throw new ArgumentException("Current employee and employer IDs cannot be both null");
+
+            var messages = await context.Messages
+                .Where(x => x.ChatId == chatId)
+                .Where(x => x.IsRead == false && x.SenderId != currentId)
+                .OrderBy(x => x.CreatedAt)
+                .Take(PaginationConstants.MessagesPageSize)
+                .ToListAsync();
+            return messages;
+        }
+
+        public async Task<List<Message>> GetLastReadMessagesAsync(Guid chatId, Guid? currentEmployerId,
+            Guid? currentEmployeeId, int pageNumber)
+        {
+            var currentId = currentEmployeeId ?? currentEmployerId;
+            if (currentId == null)
+                throw new ArgumentException("Current employee and employer IDs cannot be both null");
+
+            var messages = await context.Messages
+                .Where(x => x.ChatId == chatId)
+                .Where(x => x.IsRead == true || x.SenderId == currentId)
+                .OrderByDescending(x => x.CreatedAt)
+                .Skip((pageNumber - 1) * PaginationConstants.MessagesPageSize)
+                .Take(PaginationConstants.MessagesPageSize)
+                .ToListAsync();
+            return messages;
+        }
+
+        public async Task<int> MarkMessagesAsReadAsync(List<Message> messages)
+        {
+            int counter = 0;
+            var messageIdsToUpdate = messages.Where(x => !x.IsRead).Select(x => x.Id).ToList();
+            var messagesToUpdate = await context.Messages.Where(x => messageIdsToUpdate.Contains(x.Id)).ToListAsync();
+            foreach (var message in messagesToUpdate)
+            {
+                message.IsRead = true;
+                counter++;
+            }
+
+            await context.SaveChangesAsync();
+            return counter;
+        }
+
+        public async Task<bool> MarkMessageAsReadAsync(Guid messageId)
+        {
+            var message = await context.Messages.SingleOrDefaultAsync(x => x.Id == messageId);
+            if(message == null)
+                return false;
+            
+            message.IsRead = true;
+            await context.SaveChangesAsync();
+            return true;
+        }
     }
 }
