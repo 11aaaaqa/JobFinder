f8856355b745e9a178410b9c6b912ce60f15b63b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-03 13:55:21 +0400

    Update chat response models

--- src/Web/Web.MVC/Models/ApiResponses/Chat/ChatResponse.cs
+++ src/Web/Web.MVC/Models/ApiResponses/Chat/ChatResponse.cs
@@ -6,9 +6,11 @@
 
         public Guid EmployeeId { get; set; }
         public string EmployeeFullName { get; set; }
+        public int EmployeeUnreadMessagesCount { get; set; } = 0;
 
         public Guid EmployerId { get; set; }
         public string EmployerFullName { get; set; }
+        public int EmployerUnreadMessagesCount { get; set; } = 0;
         public DateTime LastMessageSendingTime { get; set; }
 
         public List<MessageResponse> Messages { get; set; } = new();
