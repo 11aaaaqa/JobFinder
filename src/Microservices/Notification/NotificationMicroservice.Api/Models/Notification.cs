593d4e8e0306012a72ef77cfd65380342171f2a9
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-14 16:31:10 +0400

    Add notification model

---
+++ src/Microservices/Notification/NotificationMicroservice.Api/Models/Notification.cs
@@ -0,0 +1,10 @@
+﻿namespace NotificationMicroservice.Api.Models
+{
+    public class Notification
+    {
+        public Guid Id { get; set; }
+        public string AspNetUserId { get; set; } 
+        public string Body { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
