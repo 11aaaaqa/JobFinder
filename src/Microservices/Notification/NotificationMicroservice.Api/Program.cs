3294e00fe4a0cf622476f72ff562016ca0820461
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-14 16:32:04 +0400

    Register services

--- src/Microservices/Notification/NotificationMicroservice.Api/Program.cs
+++ src/Microservices/Notification/NotificationMicroservice.Api/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using NotificationMicroservice.Api.Database;
+using NotificationMicroservice.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(x => 
+    x.UseNpgsql(builder.Configuration["Database:ConnectionString"]));
+
+builder.Services.AddTransient<INotificationService, NotificationService>();
 
 builder.Services.AddControllers();
 
