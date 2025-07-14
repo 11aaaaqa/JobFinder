a799df845a5e1ed77bf869cefd2270ea7499dabb
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-14 16:31:58 +0400

    Add notification service

---
+++ src/Microservices/Notification/NotificationMicroservice.Api/Services/NotificationService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotificationMicroservice.Api.Constants;
+using NotificationMicroservice.Api.Database;
+using NotificationMicroservice.Api.Models;
+
+namespace NotificationMicroservice.Api.Services
+{
+    public class NotificationService(ApplicationDbContext context) : INotificationService
+    {
+        public async Task<Notification?> GetNotificationByIdAsync(Guid id)
+            => await context.Notifications.SingleOrDefaultAsync(x => x.Id == id);
+
+        public async Task<List<Notification>> GetNotificationsByUserId(string aspNetUserId, int pageNumber)
+        {
+            var notifications = await context.Notifications
+                .Where(x => x.AspNetUserId == aspNetUserId)
+                .OrderByDescending(x => x.CreatedAt)
+                .Skip((pageNumber - 1) * PaginationConstants.NotificationsPageSize)
+                .Take(PaginationConstants.NotificationsPageSize)
+                .ToListAsync();
+            return notifications;
+        }
+
+        public async Task AddNotificationAsync(Notification notification)
+        {
+            await context.Notifications.AddAsync(notification);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task RemoveNotificationsAsync(List<Notification> notifications)
+        {
+            var notificationsToDelete = await context.Notifications
+                .Where(x => notifications.Contains(x))
+                .ToListAsync();
+
+            context.Notifications.RemoveRange(notificationsToDelete);
+            await context.SaveChangesAsync();
+        }
+    }
+}
+
