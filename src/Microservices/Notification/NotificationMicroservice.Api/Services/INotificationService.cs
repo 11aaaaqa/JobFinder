c4d14009b4fd360e8d81d1f801a2c4550d07a9d9
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-14 16:31:58 +0400

    Add notification service

---
+++ src/Microservices/Notification/NotificationMicroservice.Api/Services/INotificationService.cs
@@ -0,0 +1,12 @@
+﻿using NotificationMicroservice.Api.Models;
+
+namespace NotificationMicroservice.Api.Services
+{
+    public interface INotificationService
+    {
+        public Task<Notification?> GetNotificationByIdAsync(Guid id);
+        public Task<List<Notification>> GetNotificationsByUserId(string aspNetUserId, int pageNumber);
+        public Task AddNotificationAsync(Notification notification);
+        public Task RemoveNotificationsAsync(List<Notification> notifications);
+    }
+}
