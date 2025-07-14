1c8a983b59ebfc3e02c023dbad9a9cbc49017611
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-14 16:30:57 +0400

    Add DTOs

---
+++ src/Microservices/Notification/NotificationMicroservice.Api/DTOs/AddNotificationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NotificationMicroservice.Api.DTOs
+{
+    public class AddNotificationDto
+    {
+        public Guid Id { get; set; }
+        public string AspNetUserId { get; set; }
+        public string Body { get; set; }
+    }
+}
