779a6581d4436617f07f9ce9b6dd71782d8cf101
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-22 12:52:31 +0400

    Add hub connections manager service

---
+++ src/Web/Web.MVC/Services/Hub connection services/IHubConnectionsManager.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.MVC.Services.Hub_connection_services
+{
+    public interface IHubConnectionsManager
+    {
+        public void AddConnection(string userEmail, string hubConnection);
+        public string? GetConnection(string userEmail);
+        public void RemoveConnection(string userEmail);
+    }
+}
