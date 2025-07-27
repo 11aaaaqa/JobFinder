68b507f4bd6e99ba3f500ca29b1978431fdcdd76
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-27 20:10:27 +0400

    Add notifiation service

---
+++ src/Web/Web.MVC/Services/Notification services/INotificationService.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.MVC.Services.Notification_services
+{
+    public interface INotificationService
+    {
+        public Task AddNotification(string receiverUserEmail, string body);
+    }
+}
---
+++ src/Web/Web.MVC/Services/Notification services/NotificationService.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using System.Text;
+using Microsoft.AspNetCore.SignalR;
+using Web.MVC.Chat_services;
+using Web.MVC.Services.Hub_connection_services;
+
+namespace Web.MVC.Services.Notification_services
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly ILogger<NotificationService> logger;
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly IHubConnectionsManager hubConnectionsManager;
+        private readonly IHubContext<ChatHub> hubContext;
+        private readonly string url;
+        public NotificationService(IHttpClientFactory httpClientFactory, ILogger<NotificationService> logger, IConfiguration configuration,
+            IHubConnectionsManager hubConnectionsManager, IHubContext<ChatHub> hubContext)
+        {
+            this.httpClientFactory = httpClientFactory;
+            this.logger = logger;
+            this.hubConnectionsManager = hubConnectionsManager;
+            this.hubContext = hubContext;
+            url = $"{configuration["Url:Protocol"]}://{configuration["Url:Domain"]}";
+        }
+        public async Task AddNotification(string receiverUserEmail, string body)
+        {
+            using HttpClient httpClient = httpClientFactory.CreateClient();
+            Guid notificationId = Guid.NewGuid();
+            using StringContent jsonContent = new(JsonSerializer.Serialize(new
+            {
+                Id = notificationId,
+                UserEmail = receiverUserEmail,
+                Body = body
+            }), Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync($"{url}/api/Notification/AddNotification", jsonContent);
+            if (!response.IsSuccessStatusCode)
+                logger.LogCritical("Something went wrong, notification hasn't been created");
+
+            var connection = hubConnectionsManager.GetConnection(receiverUserEmail);
+            if (connection != null)
+                await hubContext.Clients.Client(connection).SendAsync("ReceiveNotification", notificationId);
+        }
+    }
+}
