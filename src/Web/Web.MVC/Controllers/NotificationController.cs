b0b06983a016ae3b8ed9c95cf654f6a96870fcb7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-28 18:55:28 +0400

    Add notification controller

---
+++ src/Web/Web.MVC/Controllers/NotificationController.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Web.MVC.Models.ApiResponses.Notification;
+
+namespace Web.MVC.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly string url;
+        public NotificationController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            this.httpClientFactory = httpClientFactory;
+            url = $"{configuration["Url:Protocol"]}://{configuration["Url:Domain"]}";
+        }
+
+        [Route("notifications/get-by-email/json")]
+        public async Task<IActionResult> GetJsonNotificationsByUserEmail(string userEmail, int pageNumber)
+        {
+            using HttpClient httpClient = httpClientFactory.CreateClient();
+
+            var notificationsResponse = await httpClient.GetAsync(
+                $"{url}/api/Notification/GetNotificationsByUserEmail?userEmail={userEmail}&pageNumber={pageNumber}");
+            notificationsResponse.EnsureSuccessStatusCode();
+            var notifications = await notificationsResponse.Content.ReadFromJsonAsync<List<NotificationResponse>>();
+
+            using StringContent jsonContent = new(JsonSerializer.Serialize(new
+            {
+                Notifications = notifications
+            }), Encoding.UTF8, "application/json");
+            var removeNotificationsResponse = await httpClient.PostAsync($"{url}/api/Notification/RemoveNotifications", jsonContent);
+            removeNotificationsResponse.EnsureSuccessStatusCode();
+
+            return new JsonResult(notifications);
+        }
+
+        [Route("notifications/remove")]
+        [HttpPost]
+        public async Task<IActionResult> RemoveNotifications([FromBody] List<NotificationResponse> notifications)
+        {
+            using HttpClient httpClient = httpClientFactory.CreateClient();
+            using StringContent jsonContent = new(JsonSerializer.Serialize(new
+            {
+                Notifications = notifications
+            }), Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync($"{url}/api/Notification/RemoveNotifications", jsonContent);
+            response.EnsureSuccessStatusCode();
+
+            return Ok();
+        }
+
+        [Route("notifications/get-notifications-count-by-user-email")]
+        public async Task<IActionResult> GetJsonNotificationsCountByUserEmail(string userEmail)
+        {
+            using HttpClient httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync(
+                $"{url}/api/Notification/GetNotificationsCountByUserEmail?userEmail={userEmail}");
+            response.EnsureSuccessStatusCode();
+            int notificationsCount = await response.Content.ReadFromJsonAsync<int>();
+
+            return new JsonResult(notificationsCount);
+        }
+
+        [Route("notifications/{notificationId}")]
+        public async Task<IActionResult> GetNotificationByIdJson(Guid notificationId)
+        {
+            using HttpClient httpsClient = httpClientFactory.CreateClient();
+
+            var notificationResponse = await httpsClient.GetAsync($"{url}/api/Notification/GetNotificationById/{notificationId}");
+            notificationResponse.EnsureSuccessStatusCode();
+            var notification = await notificationResponse.Content.ReadFromJsonAsync<NotificationResponse>();
+
+            return new JsonResult(notification);
+        }
+    }
+}
