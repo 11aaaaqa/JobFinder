ec9ad55068ead912ed135242d4fe71445a5fe7e7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-08-19 20:40:49 +0400

    Add review model

---
+++ src/Microservices/Review/ReviewMicroservice.Api/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿namespace ReviewMicroservice.Api.Models
+{
+    public class Review
+    {
+        public Guid Id { get; set; }
+        public Guid CompanyId { get; set; }
+        public Guid EmployeeId { get; set; }
+        public string Position { get; set; }
+        public string WorkingState { get; set; }
+        public string WorkingTime { get; set; }
+        public string Advantages { get; set; }
+        public string CanBeImproved { get; set; }
+        public byte WorkingConditions { get; set; }
+        public byte Colleagues { get; set; }
+        public byte Management { get; set; }
+        public byte GrowthOpportunities { get; set; }
+        public byte RestConditions { get; set; }
+        public byte SalaryLevel { get; set; }
+        public byte GeneralEstimation { get; set; }
+        public DateOnly CreatedAt { get; set; }
+    }
+}
