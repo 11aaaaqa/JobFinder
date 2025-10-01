5e94a5b080381e4955f1aa39f2dbb283feb77fae
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-10-01 18:10:02 +0400

    Add review service

---
+++ src/Microservices/Review/ReviewMicroservice.Api/Services/ReviewRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewMicroservice.Api.Constants;
+using ReviewMicroservice.Api.Database;
+using ReviewMicroservice.Api.Models;
+
+namespace ReviewMicroservice.Api.Services
+{
+    public class ReviewRepository(ApplicationDbContext context) : IReviewRepository
+    {
+        public async Task<List<Review>> GetReviewsByEmployeeIdAsync(Guid employeeId, int pageNumber)
+        {
+            var reviews = await context.Reviews
+                .Where(x => x.EmployeeId == employeeId)
+                .Skip((pageNumber - 1) * PaginationConstants.ReviewsPageSize)
+                .Take(PaginationConstants.ReviewsPageSize)
+                .ToListAsync();
+            return reviews;
+        }
+
+        public async Task<List<Review>> GetReviewsByCompanyIdAsync(Guid companyId, int pageNumber)
+        {
+            var reviews = await context.Reviews
+                .Where(x => x.CompanyId == companyId)
+                .Skip((pageNumber - 1) * PaginationConstants.ReviewsPageSize)
+                .Take(PaginationConstants.ReviewsPageSize)
+                .ToListAsync();
+            return reviews;
+        }
+
+        public async Task<Review> GetByIdAsync(Guid reviewId)
+            => await context.Reviews.SingleAsync(x => x.Id == reviewId);
+
+        public async Task AddReviewAsync(Review review)
+        {
+            await context.Reviews.AddAsync(review);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteReviewAsync(Guid reviewId)
+        {
+            var reviewToDelete = await context.Reviews.SingleOrDefaultAsync(x => x.Id == reviewId);
+            if (reviewToDelete != null)
+                context.Reviews.Remove(reviewToDelete);
+        }
+    }
+}
