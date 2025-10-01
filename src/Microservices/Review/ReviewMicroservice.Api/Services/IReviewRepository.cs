aec90b2b415ef2ceba7497a40cb71f5566219024
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-10-01 18:10:02 +0400

    Add review service

---
+++ src/Microservices/Review/ReviewMicroservice.Api/Services/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReviewMicroservice.Api.Models;
+
+namespace ReviewMicroservice.Api.Services
+{
+    public interface IReviewRepository
+    {
+        Task<List<Review>> GetReviewsByEmployeeIdAsync(Guid employeeId, int pageNumber);
+        Task<List<Review>> GetReviewsByCompanyIdAsync(Guid companyId, int pageNumber);
+        Task<Review> GetByIdAsync(Guid reviewId);
+        Task AddReviewAsync(Review review);
+        Task DeleteReviewAsync(Guid reviewId);
+    }
+}
