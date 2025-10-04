9c6465cd320bbee9c592bab0717fe3877ff712a8
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-10-04 23:05:56 +0400

    Update services

--- src/Microservices/Review/ReviewMicroservice.Api/Services/IReviewRepository.cs
+++ src/Microservices/Review/ReviewMicroservice.Api/Services/IReviewRepository.cs
@@ -4,8 +4,10 @@ namespace ReviewMicroservice.Api.Services
 {
     public interface IReviewRepository
     {
-        Task<List<Review>> GetReviewsByEmployeeIdAsync(Guid employeeId, int pageNumber);
-        Task<List<Review>> GetReviewsByCompanyIdAsync(Guid companyId, int pageNumber);
+        Task<List<Review>> GetAllReviewsByCompanyIdAsync(Guid companyId);
+        Task<List<Review>> GetAllReviewsByEmployeeIdAsync(Guid employeeId);
+        Task<List<Review>> GetReviewsByEmployeeIdPaginationAsync(Guid employeeId, int pageNumber);
+        Task<List<Review>> GetReviewsByCompanyIdPaginationAsync(Guid companyId, int pageNumber);
         Task<Review> GetByIdAsync(Guid reviewId);
         Task AddReviewAsync(Review review);
         Task DeleteReviewAsync(Guid reviewId);
