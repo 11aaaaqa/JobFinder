3243b1deb2c5c62a98746793f3be55fb310d4380
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-10-07 13:50:05 +0400

    Add migration

---
+++ src/Microservices/Review/ReviewMicroservice.Api/Migrations/20251007094847_Initial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReviewMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CompanyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    EmployeeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Position = table.Column<string>(type: "text", nullable: false),
+                    City = table.Column<string>(type: "text", nullable: false),
+                    WorkingState = table.Column<string>(type: "text", nullable: false),
+                    WorkingTime = table.Column<string>(type: "text", nullable: false),
+                    Advantages = table.Column<string>(type: "text", nullable: false),
+                    CanBeImproved = table.Column<string>(type: "text", nullable: false),
+                    WorkingConditions = table.Column<int>(type: "integer", nullable: false),
+                    Colleagues = table.Column<int>(type: "integer", nullable: false),
+                    Management = table.Column<int>(type: "integer", nullable: false),
+                    GrowthOpportunities = table.Column<int>(type: "integer", nullable: false),
+                    RestConditions = table.Column<int>(type: "integer", nullable: false),
+                    SalaryLevel = table.Column<int>(type: "integer", nullable: false),
+                    GeneralEstimation = table.Column<double>(type: "double precision", nullable: false),
+                    CreatedAt = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+        }
+    }
+}
