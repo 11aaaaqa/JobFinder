bfdf19641db93dba1b0e3870625e554cc9bb1b27
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-10-12 15:02:37 +0400

    Register services

--- src/Microservices/Review/ReviewMicroservice.Api/Program.cs
+++ src/Microservices/Review/ReviewMicroservice.Api/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using ReviewMicroservice.Api.Database;
+using ReviewMicroservice.Api.Kafka.Producer;
+using ReviewMicroservice.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(
     x => x.UseNpgsql(builder.Configuration["Database:ConnectionString"]));
+
+builder.Services.AddScoped<IKafkaProducer, KafkaProducer>();
+builder.Services.AddTransient<IReviewRepository, ReviewRepository>();
 
 builder.Services.AddControllers();
 
