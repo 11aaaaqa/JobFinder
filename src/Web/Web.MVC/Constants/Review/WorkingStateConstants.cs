ce5aa526488aac29aee1234de1dbc243e8631e9d
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-10-13 21:56:02 +0400

    Add constants

---
+++ src/Web/Web.MVC/Constants/Review/WorkingStateConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.Constants.Review
+{
+    public class WorkingStateConstants
+    {
+        public const string CurrentJob = "Текущее место работы";
+        public const string PreviousJob = "Прошлое место работы";
+    }
+}
