ec097e836808784f999f32d80ee50c6e0743dd35
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-10-13 21:56:16 +0400

    Register pagination service

--- src/Microservices/Review/ReviewMicroservice.Api/Program.cs
+++ src/Microservices/Review/ReviewMicroservice.Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using ReviewMicroservice.Api.Database;
 using ReviewMicroservice.Api.Kafka.Producer;
 using ReviewMicroservice.Api.Services;
+using ReviewMicroservice.Api.Services.Pagination;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(
 
 builder.Services.AddScoped<IKafkaProducer, KafkaProducer>();
 builder.Services.AddTransient<IReviewRepository, ReviewRepository>();
+builder.Services.AddTransient<IPaginationService, PaginationService>();
 
 builder.Services.AddControllers();
 
