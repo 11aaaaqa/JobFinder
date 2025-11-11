e767a617f188e3a2c20420c2d46694b05eb76918
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-11-11 19:38:57 +0400

    Add account type authorization filter

---
+++ src/Web/Web.MVC/Filters/Authorization filters/Account type filters/AccountTypeAuthorizationFilterService.cs
@@ -0,0 +1,43 @@
+﻿using GeneralLibrary.Constants;
+using GeneralLibrary.Enums;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Web.MVC.Models.ApiResponses;
+
+namespace Web.MVC.Filters.Authorization_filters.Account_type_filters
+{
+    public class AccountTypeAuthorizationFilterService(IHttpClientFactory httpClientFactory, IConfiguration configuration, AccountTypeEnum accountType)
+        : IAsyncAuthorizationFilter
+    {
+        private readonly string url = $"{configuration["Url:Protocol"]}://{configuration["Url:Domain"]}";
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            string? currentUserEmail = context.HttpContext.User.Identity.Name;
+            if (currentUserEmail == null)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            HttpClient httpClient = httpClientFactory.CreateClient();
+
+            var currentUserResponse = await httpClient.GetAsync($"{url}/api/User/GetUserByEmail/{currentUserEmail}");
+            if (!currentUserResponse.IsSuccessStatusCode)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+            var currentUser = await currentUserResponse.Content.ReadFromJsonAsync<IdentityUserResponse>();
+
+            switch (accountType)
+            {
+                case AccountTypeEnum.Employee:
+                    if (currentUser.AccountType != AccountTypeConstants.Employee) context.Result = new ForbidResult();
+                    break;
+                case AccountTypeEnum.Employer:
+                    if (currentUser.AccountType != AccountTypeConstants.Employer) context.Result = new ForbidResult();
+                    break;
+            }
+        }
+    }
+}
